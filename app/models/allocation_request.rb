6286bfaabb4b5f9048eccbf3fdf17db0eb3a2eac
Al Davidson <apdavidson@gmail.com>
2020-05-20 12:45:09 +0100

    add round1 form

---
+++ app/models/allocation_request.rb
@@ -0,0 +1,2 @@
+class AllocationRequest < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
 
-  resources :application_forms
+  resources :application_forms, :allocation_request_forms
 
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
 
-  get '/', to: redirect('application_forms/new')
+  get '/', to: redirect('allocation_request_forms/new')
 end
