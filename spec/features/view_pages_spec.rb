c26a6f05dbed64481a40c265933a4a6e26e8ffc2
Al Davidson <apdavidson@gmail.com>
2020-06-03 10:35:50 +0100

    Guidance page updates from Emma (pre-spad approval

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'View pages', type: :feature do
-  scenario 'Navigate to home' do
-    visit '/pages/home'
+  scenario 'Root URL should be the guidance page' do
+    visit '/'
+    expect(page).to have_text 'Improving children’s internet access'
+  end
 
-    expect(page).to have_text('Lorem')
+  scenario 'Navigate to guidance page' do
+    visit '/pages/guidance'
+
+    expect(page).to have_http_status(:ok)
+    expect(page).to have_text 'Improving children’s internet access'
   end
 end
