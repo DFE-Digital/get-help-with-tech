3f9aa84355e313110769c1d8015b270a62b66d96
Al Davidson <apdavidson@gmail.com>
2020-06-24 16:35:31 +0100

    add sign_in_link back in

--- app/mailers/sign_in_token_mailer.rb
+++ app/mailers/sign_in_token_mailer.rb
@@ -9,6 +9,7 @@ class SignInTokenMailer < ApplicationMailer
       to: @user.email_address,
       token_url: url(:validate_sign_in_token_url, token: @user.sign_in_token, identifier: identifier),
       token_expires_at: I18n.l(@user.sign_in_token_expires_at, format: :time_only),
+      sign_in_link: url(:sign_in_url),
     }
 
     template_mail(
