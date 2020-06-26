14e775dc5605d93aaa4e1f17fc61903cc7539d94
Al Davidson <apdavidson@gmail.com>
2020-06-26 14:54:03 +0100

    correct canonical host, and apply CSP

--- config/initializers/canonical_rails.rb
+++ config/initializers/canonical_rails.rb
@@ -7,7 +7,7 @@ CanonicalRails.setup do |config|
 
   # This is the main host, not just the TLD, omit slashes and protocol. If you have more than one, pick the one you want to rank in search results.
 
-  config.host = "www.education.gov.uk"
+  config.host = ENV['HOSTNAME_FOR_URLS'] || 'get-help-with-tech.education.gov.uk'
   config.port# = '3000'
 
   # http://en.wikipedia.org/wiki/URL_normalization
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,7 +9,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
   policy.object_src  :none
-  policy.script_src  :self, :https
+  policy.script_src  :self
   policy.style_src   :self, :https
   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
