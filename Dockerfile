5051f86960ca0ef602f4f450de6adc22e86e0ebd
Al Davidson <apdavidson@gmail.com>
2020-06-26 13:42:15 +0100

    bake git commit & branch in to the docker container, add healthcheck.json endpoint which reports them as well as db status

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ RUN echo "deb https://dl.yarnpkg.com/debian/ stable main" |  tee /etc/apt/source
 
 RUN apt-get update && apt-get install -y nodejs postgresql-contrib libpq-dev yarn
 
-
 ENV RAILS_ROOT /var/www/${APPNAME}
 RUN mkdir -p $RAILS_ROOT
 WORKDIR $RAILS_ROOT
@@ -25,17 +24,13 @@ RUN gem install bundler
 RUN chown -R deploy:deploy /usr/local/bundle/
 USER 1001
 
-
 # install all gems
 COPY --chown=deploy:deploy Gemfile Gemfile.lock .ruby-version ./
 ARG BUNDLE_FLAGS="--jobs 2 --no-cache --without development test"
 RUN bundle install ${BUNDLE_FLAGS}
 RUN bundle package --all
 
-
 COPY --chown=deploy:deploy . .
-
-
 
 # allow access to port 3000
 ENV APP_PORT 3000
@@ -45,6 +40,13 @@ EXPOSE $APP_PORT
 RUN yarn
 ARG RAILS_ENV=production
 RUN RAILS_ENV=${RAILS_ENV} SECRET_KEY_BASE=$(bin/rake secret) bundle exec rails webpacker:compile
+
+# Cache the git commit sha & branch
+ARG GIT_COMMIT_SHA=""
+ARG GIT_BRANCH=""
+ENV GIT_COMMIT_SHA=${GIT_COMMIT_SHA}
+ENV GIT_BRANCH=${GIT_BRANCH}
+RUN echo "[{'commit_sha': '${GIT_COMMIT_SHA}', 'branch': '${GIT_BRANCH}'}]" > ./.gitinfo.json
 
 # run the rails server
 ARG RAILS_ENV=production
---
+++ spec/controllers/monitoring_controller_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+RSpec.describe MonitoringController, type: :controller do
+  describe 'healthcheck.json' do
+    let(:json) do
+      JSON.parse(response.body)
+    end
+
+    it 'returns json' do
+      get :healthcheck, format: :json
+      expect(response.media_type).to eq('application/json')
+    end
+
+    context 'when everything is OK' do
+      it 'has status ok' do
+        get :healthcheck, format: :json
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when not everything is OK' do
+      before do
+        allow(HealthcheckService).to receive(:db_status).and_return('DOWN')
+      end
+
+      it 'has status internal_server_error' do
+        get :healthcheck, format: :json
+        expect(response.status).to eq(500)
+      end
+    end
+
+    context 'when GIT_BRANCH is present in the ENV' do
+      before do
+        ENV['GIT_BRANCH'] = 'my-branch-name'
+      end
+
+      it 'reports GIT_BRANCH in the info/git/branch key' do
+        get :healthcheck, format: :json
+
+        expect(json['info']['git']['branch']).to eq('my-branch-name')
+      end
+    end
+
+    context 'when GIT_BRANCH is not present in the ENV' do
+      before do
+        ENV['GIT_BRANCH'] = nil
+      end
+
+      it 'reports null in the info/git/branch key' do
+        get :healthcheck, format: :json
+
+        expect(json['info']['git']['branch']).to eq(nil)
+      end
+    end
+
+    context 'when GIT_COMMIT_SHA is present in the ENV' do
+      before do
+        ENV['GIT_COMMIT_SHA'] = 'my-commit-sha'
+      end
+
+      it 'reports GIT_COMMIT_SHA in the info/git/commit_sha key' do
+        get :healthcheck, format: :json
+
+        expect(json['info']['git']['commit_sha']).to eq('my-commit-sha')
+      end
+    end
+
+    context 'when GIT_COMMIT_SHA is not present in the ENV' do
+      before do
+        ENV['GIT_COMMIT_SHA'] = nil
+      end
+
+      it 'reports null in the info/git/commit_sha key' do
+        get :healthcheck, format: :json
+
+        expect(json['info']['git']['commit_sha']).to eq(nil)
+      end
+    end
+  end
+end
