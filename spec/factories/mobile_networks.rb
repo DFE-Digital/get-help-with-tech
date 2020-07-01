464395f81426ee587589606c26cbfc4e1f20cb44
Jake Benilov <benilov@gmail.com>
2020-07-01 18:08:58 +0100

    Make factory_bot factories work with multiple objects
    Before this change, the code was hitting validations.
    
    Also, extract approved and not_approved traits in the user factory.

--- spec/factories/mobile_networks.rb
+++ spec/factories/mobile_networks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :mobile_network, class: 'MobileNetwork' do
-    brand                   { 'Participating mobile network' }
+    sequence(:brand)        { |n| "Participating mobile network #{n}" }
     host_network            { 'AA' }
     participating_in_scheme { true }
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
-  factory :local_authority_user, class: 'User' do
-    full_name     { 'Jane Doe' }
-    email_address { 'jane.doe@somelocalauthority.gov.uk' }
-    organisation  { 'Some Local Authority' }
+  trait :approved do
     approved_at   { Time.now.utc - 3.days }
+  end
+
+  trait :not_approved do
+    approved_at   { nil }
+  end
+
+  factory :local_authority_user, class: 'User' do
+    full_name                { 'Jane Doe' }
+    sequence(:email_address) { |n| "jane.doe#{n}@somelocalauthority.gov.uk" }
+    organisation             { 'Some Local Authority' }
+    approved
   end
 
   factory :mno_user, class: 'User' do
