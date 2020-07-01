07c0ae4345a03226df79216a72ce6d1cbd014fac
Jake Benilov <benilov@gmail.com>
2020-07-01 16:19:05 +0100

    Method for assigning unallocated vouchers to responsible bodies

--- app/models/bt_wifi_voucher.rb
+++ app/models/bt_wifi_voucher.rb
@@ -3,4 +3,10 @@ class BTWifiVoucher < ApplicationRecord
     class_name: 'ResponsibleBody',
     foreign_key: 'distributed_to_responsible_body_id',
     optional: true
+
+  def self.assign(number, to:)
+    self.where(distributed_to: nil)
+      .take(number)
+      .map {|responsible_body| responsible_body.update(distributed_to: to, distributed_at: Time.zone.now) }
+  end
 end
