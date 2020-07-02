b63c066d54b913eed1757be6b35479f437ed3c6c
Misha Gorodnitzky <misaka@pobox.com>
2020-07-02 23:08:43 +0100

    [219] require sign-in on /responsible_body page

--- app/controllers/responsible_bodies_controller.rb
+++ app/controllers/responsible_bodies_controller.rb
@@ -1,9 +1,10 @@
 class ResponsibleBodiesController < ApplicationController
+  before_action :require_sign_in!
   before_action :build_reponsible_body
 
   def show; end
 
-  private
+private
 
   def build_reponsible_body
     # @responsible_body = ResponsibleBody.find(@user.responsible_body_id)
--- spec/features/responsible_body_spec.rb
+++ spec/features/responsible_body_spec.rb
@@ -1,9 +1,28 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.feature ResponsibleBody do
-  scenario 'visiting the page' do
-    visit responsible_body_path
+  let(:sign_in_page) { PageObjects::SignInPage.new }
+  let(:responsible_body_page) { PageObjects::ResponsibleBodyPage.new }
+  let(:user) { create :local_authority_user }
 
-    expect(page.status_code).to eq 200
+  context "user that isn't signed in" do
+    scenario 'visiting the page' do
+      visit responsible_body_path
+
+      expect(sign_in_page).to be_displayed
+    end
+  end
+
+  context 'signed-in user' do
+    before do
+      sign_in_as user
+    end
+
+    scenario 'visiting the page' do
+      visit responsible_body_path
+
+      expect(responsible_body_page).to be_displayed
+      expect(page.status_code).to eq 200
+    end
   end
 end
