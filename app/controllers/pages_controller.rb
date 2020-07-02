11e162e85ab72d6e513106168a129a078056631c
Paul Hayes <fofr@users.noreply.github.com>
2020-07-02 16:16:55 +0100

    Put MNO guidance behind feature flag
    Only show the guidance when we disable the "static_guidance_only" flag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
   def bt_wifi_privacy_notice; end
 
   def suggested_email_to_schools; end
+
+  def about_increasing_mobile_data
+    if FeatureFlag.active?(:static_guidance_only)
+      render 'errors/not_found', status: :not_found
+    end
+  end
 end
