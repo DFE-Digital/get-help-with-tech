aee3dec4fcff76352c18c9c4bac67481d6f32ab9
Paul Hayes <fofr@users.noreply.github.com>
2020-07-02 13:53:06 +0100

    Add static guidance about increasing mobile data
    This doesnt include the "get involved" section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/about-bt-wifi', to: 'pages#about_bt_wifi', as: :guidance_page
+  get '/about-increasing-mobile-data', to: 'pages#about_increasing_mobile_data'
   get '/bt-wifi/privacy-notice', to: 'pages#bt_wifi_privacy_notice'
   get '/bt-wifi/suggested-email-to-schools', to: 'pages#suggested_email_to_schools'
   get '/pages/guidance', to: redirect('/about-bt-wifi')
