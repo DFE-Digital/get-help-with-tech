5ebfc940f4ed1dd9d1f7379b3c9cda524f9de5e8
Al Davidson <apdavidson@gmail.com>
2020-07-03 12:20:56 +0100

    Revert "fix broken Trust import"
    This reverts commit 1ac0999b49954e04b0aa380d3d1f57dedd2d5e13.

--- app/models/trust.rb
+++ app/models/trust.rb
@@ -1,6 +1,6 @@
 class Trust < ResponsibleBody
   enum organisation_type: {
     multi_academy_trust: 'Multi-academy trust',
-    single_academy_trust: 'Single-academy trust',
+    single_academy_trust: 'Single academy trust',
   }
 end
