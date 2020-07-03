a74c449fb7ecd1896589319d4f1428e880afe502
Jake Benilov <benilov@gmail.com>
2020-07-03 16:42:53 +0100

    Add start page and incorporate it in the feature spec

--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -10,13 +10,15 @@ RSpec.feature 'Creating an account', type: :feature do
     end
 
     scenario 'clicking sign in shows option to create an account' do
-      visit sign_in_path
+      visit start_path
+      click_on 'Start'
       expect(page).to have_content('Do you already have an account?')
       expect(page).to have_content('No, I need to create an account')
     end
 
     scenario 'supplying valid info sends a token via email' do
-      visit sign_in_path
+      visit start_path
+      click_on 'Start'
       find('#sign-in-token-form-already-have-account-no-field').choose
       click_on('Continue')
 
