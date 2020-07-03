bb8a4c5e138bc4d2ba645087e9662d1d09b09474
Al Davidson <apdavidson@gmail.com>
2020-07-03 08:12:11 +0100

    feature spec for RB ExtraMobileDataRequest index

--- app/models/local_authority.rb
+++ app/models/local_authority.rb
@@ -1,2 +1,3 @@
 class LocalAuthority < ResponsibleBody
+  ORGANISATION_TYPES = %w[COMB NMD MD UA SRA CC CTY LBO].freeze
 end
--- app/models/trust.rb
+++ app/models/trust.rb
@@ -1,2 +1,3 @@
 class Trust < ResponsibleBody
+  ORGANISATION_TYPES = ['Single academy trust', 'Multi-academy trust'].freeze
 end
