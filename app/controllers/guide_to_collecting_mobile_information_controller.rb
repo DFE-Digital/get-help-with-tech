484e4567d2f66c1787b7fed55ad54592ce582753
Paul Hayes <fofr@users.noreply.github.com>
2020-07-03 14:40:09 +0100

    Change guide path to friendlier one

---
+++ app/controllers/guide_to_collecting_mobile_information_controller.rb
@@ -0,0 +1,51 @@
+class GuideToCollectingMobileInformationController < ApplicationController
+  include Rails.application.routes.url_helpers
+  before_action :set_guide_navigation
+
+  def index; end
+
+  def asking_for_network; end
+
+  def asking_for_account_holder; end
+
+  def telling_about_offer; end
+
+  def privacy; end
+
+private
+
+  def set_guide_navigation
+    @contents = contents
+    current_index = @contents.find_index { |item| item[:path] == request.path }
+
+    @contents[current_index][:active] = true
+    @next_page = contents[current_index + 1] || false
+    @prev_page = contents[current_index - 1] || false
+    @title = contents[current_index][:text]
+  end
+
+  def contents
+    [
+      {
+        text: t('guide_to_collecting_mobile_information.overview'),
+        path: guide_to_collecting_mobile_information_path,
+      },
+      {
+        text: t('guide_to_collecting_mobile_information.asking_for_network'),
+        path: guide_to_collecting_mobile_information_asking_for_network_path,
+      },
+      {
+        text: t('guide_to_collecting_mobile_information.telling_about_offer'),
+        path: guide_to_collecting_mobile_information_telling_about_offer_path,
+      },
+      {
+        text: t('guide_to_collecting_mobile_information.asking_for_account_holder'),
+        path: guide_to_collecting_mobile_information_asking_for_account_holder_path,
+      },
+      {
+        text: t('guide_to_collecting_mobile_information.privacy'),
+        path: guide_to_collecting_mobile_information_privacy_path,
+      },
+    ]
+  end
+end
