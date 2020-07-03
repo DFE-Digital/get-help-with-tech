ff9e63a693b34cf52bd5e2c1fa9a293c861ba748
Misha Gorodnitzky <misaka@pobox.com>
2020-07-03 17:15:25 +0100

    [219] validate local authorities and trusts have organisation_type

--- app/models/trust.rb
+++ app/models/trust.rb
@@ -3,4 +3,6 @@ class Trust < ResponsibleBody
     multi_academy_trust: 'Multi-academy trust',
     single_academy_trust: 'Single-academy trust',
   }
+
+  validates :organisation_type, presence: true
 end
