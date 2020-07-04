4d07c67f231252a4afba4780669eb8cdf171fa56
Al Davidson <apdavidson@gmail.com>
2020-07-04 07:12:55 +0100

    make data migration just set specific values

--- db/migrate/20200703123524_populate_mobile_network_participation_in_pilot.rb
+++ db/migrate/20200703123524_populate_mobile_network_participation_in_pilot.rb
@@ -1,7 +1,7 @@
 class PopulateMobileNetworkParticipationInPilot < ActiveRecord::Migration[6.0]
   def up
-    MobileNetwork.where(participating_in_scheme: false).update_all(participation_in_pilot: :no)
-    MobileNetwork.where(participating_in_scheme: true).update_all(participation_in_pilot: :yes)
+    MobileNetwork.where(participating_in_scheme: false).update_all(participation_in_pilot: 'no')
+    MobileNetwork.where(participating_in_scheme: true).update_all(participation_in_pilot: 'yes')
   end
 
   def down
