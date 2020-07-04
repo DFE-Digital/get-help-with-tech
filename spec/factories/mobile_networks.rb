c667e8cc63de31fe0a0f4025787b25dbd072137e
Al Davidson <apdavidson@gmail.com>
2020-07-04 07:56:46 +0100

    simpler enum value population

--- spec/factories/mobile_networks.rb
+++ spec/factories/mobile_networks.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :mobile_network, class: 'MobileNetwork' do
     sequence(:brand)       { |n| "Participating mobile network #{n}" }
     host_network           { 'AA' }
-    participation_in_pilot { MobileNetwork.participation_in_pilots.key('yes') }
+    participation_in_pilot { :participating }
 
     trait :not_participating_in_pilot do
-      participation_in_pilot { MobileNetwork.participation_in_pilots.key('no') }
+      participation_in_pilot { :not_participating }
     end
 
     trait :maybe_participating_in_pilot do
-      participation_in_pilot { MobileNetwork.participation_in_pilots.key('maybe') }
+      participation_in_pilot { :maybe_participating }
     end
   end
 end
