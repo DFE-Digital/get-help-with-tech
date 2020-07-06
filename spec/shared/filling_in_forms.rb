0146936edb2bdabb1373d5f60e24b34dee71ef56
Paul Hayes <fofr@users.noreply.github.com>
2020-07-06 11:49:12 +0100

    Tweak how we capture privacy statement
    We don't need acceptance, just needs to be read to them

--- spec/shared/filling_in_forms.rb
+++ spec/shared/filling_in_forms.rb
@@ -3,5 +3,5 @@ def fill_in_valid_application_form(mobile_network_name: 'Participating mobile ne
   fill_in 'Mobile phone number', with: '07123456789'
 
   choose mobile_network_name
-  check('Yes, they agree with the privacy statement')
+  check('Yes, the privacy statement has been shared')
 end
