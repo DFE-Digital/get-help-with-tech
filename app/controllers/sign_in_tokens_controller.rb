1c4cda14370ae984839bc2e275f520b16149ddd4
Al Davidson <apdavidson@gmail.com>
2020-07-09 09:55:44 +0100

    use find_by rather than where(...).first
    Co-authored-by: Jake Benilov <benilov@gmail.com>

--- app/controllers/sign_in_tokens_controller.rb
+++ app/controllers/sign_in_tokens_controller.rb
@@ -14,7 +14,7 @@ class SignInTokensController < ApplicationController
     flash.notice = "Welcome, #{@user.full_name}"
     redirect_to root_url_for(@user)
   rescue ArgumentError
-    user = User.where(sign_in_token: params[:token]).first
+    user = User.find_by(sign_in_token: params[:token])
     if user && user.token_is_valid_but_expired?(token: params[:token], identifier: params[:identifier])
       render :token_is_valid_but_expired, status: :bad_request
     else
