3ba00fdaa09e76171dd33fecd1e8b1924318f81e
Jake Benilov <benilov@gmail.com>
2020-07-10 11:12:40 +0100

    Break up spec into more granular specs

--- spec/services/import_bt_wifi_vouchers_service_spec.rb
+++ spec/services/import_bt_wifi_vouchers_service_spec.rb
@@ -1,29 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ImportBTWifiVouchersService, type: :model do
-  it 'imports BT Wifi vouchers from an online CSV file and remembers who they were distributed to' do
-    local_authority = create(:local_authority, name: 'Brent')
-    trust = create(:trust, name: 'SUNSHINE TRUST')
-
-    data = [
+  let!(:local_authority) { create(:local_authority, name: 'Brent') }
+  let!(:trust) { create(:trust, name: 'SUNSHINE TRUST') }
+  let(:import_data_csv) do
+    [
       'Username,Password,Assigned to responsible body name',
       'aaa,bbb,',
       'ccc,ddd,Brent',
       'eee,fff,SUNSHINE TRUST',
       'ggg,hhh,SUNSHINE TRUST',
     ].join("\n")
+  end
 
+  before do
     stub_request(:get, 'https://example.com/bt-vouchers.csv')
-      .to_return(body: data)
+      .to_return(body: import_data_csv)
 
     ImportBTWifiVouchersService.new('https://example.com/bt-vouchers.csv').call
+  end
 
+  it 'imports all the vouchers from the CSV' do
     expect(BTWifiVoucher.count).to eq(4)
+  end
 
+  it 'assigns the vouchers to the right responsible bodies' do
     expect(trust.bt_wifi_vouchers.count).to eq(2)
+    expect(local_authority.bt_wifi_vouchers.count).to eq(1)
+  end
+
+  it 'imports unassigned vouchers as unassigned' do
+    expect(BTWifiVoucher.unassigned.count).to eq(1)
+  end
+
+  it 'imports usernames & passwords correctly' do
     expect(trust.bt_wifi_vouchers.order('username asc').pluck(:username)).to eq(%w[eee ggg])
     expect(trust.bt_wifi_vouchers.order('username asc').pluck(:password)).to eq(%w[fff hhh])
-    expect(local_authority.bt_wifi_vouchers.count).to eq(1)
-    expect(BTWifiVoucher.unassigned.count).to eq(1)
   end
 end
