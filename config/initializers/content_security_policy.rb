32c2cca91f898672eb6e3dc4787fd6f1727a0a16
Paul Hayes <fofr@users.noreply.github.com>
2020-07-16 11:13:00 +0100

    Use a nonce to make inline script secure
    Our js enabled or not enabled script isn't running because we don't allow unsafe inline scripts to execute. This means Accordions don't work correctly.
    
    Add a secure `nonce` attribute which we generate on each request
    
    Followed guide here:
    https://edgeguides.rubyonrails.org/security.html

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -24,6 +24,8 @@ end
 # Set the nonce only to specific directives
 # Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
 
+Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
