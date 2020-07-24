341c2e89bf83da87bfd7c3a980be0afa527937a7
Jake Benilov <benilov@gmail.com>
2020-07-24 21:50:45 +0100

    Rewrite the RB extra mobile data requests feature spec to be more user-oriented

--- spec/features/responsible_body/extra_mobile_data_requests_spec.rb
+++ spec/features/responsible_body/extra_mobile_data_requests_spec.rb
@@ -1,47 +1,34 @@
 require 'rails_helper'
 
-RSpec.feature 'RB ExtraMobileDataRequests view', type: :feature do
+RSpec.feature 'Accessing the extra mobile data requests area as a responsible body user', type: :feature do
   let(:rb_user) { create(:local_authority_user) }
 
-  describe 'logged in as an RB user' do
+  before do
+    sign_in_as rb_user
+  end
+
+  scenario 'the user can access the request form from the responsible body home page' do
+    click_on 'Request extra data for mobile devices'
+
+    expect(page).to have_css('h1', text: 'Request extra mobile data')
+    expect(page).to have_http_status(:ok)
+    expect(page).to have_link('Request data for someone')
+  end
+
+  context 'when the user has already submitted requests' do
     before do
-      sign_in_as rb_user
+      @requests = create_list(:extra_mobile_data_request, 5, status: 'requested', created_by_user: rb_user)
     end
 
-    scenario 'visiting the index works' do
+    scenario 'the user can see their previous requests' do
       visit responsible_body_extra_mobile_data_requests_path
 
-      expect(page).to have_css('h1', text: 'Request extra mobile data')
-      expect(page).to have_http_status(:ok)
-    end
+      expect(page).to have_css('h2', text: 'Your requests')
 
-    context 'when the user has submitted requests' do
-      let(:requests) { create_list(:extra_mobile_data_request, 5, status: 'requested', created_by_user: rb_user) }
-
-      before do
-        requests
+      @requests.each do |request|
+        expect(page).to have_content(request.device_phone_number)
+        expect(page).to have_content(request.account_holder_name)
       end
-
-      scenario 'the index page shows the users requests' do
-        visit responsible_body_extra_mobile_data_requests_path
-
-        expect(page).to have_css('h2', text: 'Your requests')
-      end
-
-      scenario 'the index page shows those requests' do
-        visit responsible_body_extra_mobile_data_requests_path
-
-        requests.each do |request|
-          expect(page).to have_content(request.device_phone_number)
-          expect(page).to have_content(request.account_holder_name)
-        end
-      end
-    end
-
-    scenario 'the index page shows a button for requesting more data' do
-      visit responsible_body_extra_mobile_data_requests_path
-
-      expect(page).to have_link('Request data for someone')
     end
   end
 end
