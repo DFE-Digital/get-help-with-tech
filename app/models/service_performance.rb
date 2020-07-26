ba6d7bdab943c8a1830ddb4103b9a355a449bf9e
Jake Benilov <benilov@gmail.com>
2020-07-26 22:40:13 +0100

    DfE users can view MNO request stats in the support area

--- app/models/service_performance.rb
+++ app/models/service_performance.rb
@@ -36,4 +36,21 @@ class ServicePerformance
       .pluck(:responsible_body_id)
       .size
   end
+
+  def total_extra_mobile_data_requests
+    ExtraMobileDataRequest.count
+  end
+
+  def extra_mobile_data_requests_by_status
+    ExtraMobileDataRequest.group(:status).count
+  end
+
+  def extra_mobile_data_requests_by_mobile_network_brand
+    ExtraMobileDataRequest
+      .joins(:mobile_network)
+      .group('mobile_networks.brand')
+      .count
+      .sort_by { |_k, v| v }
+      .reverse
+  end
 end
--- spec/features/support/viewing_service_performance_spec.rb
+++ spec/features/support/viewing_service_performance_spec.rb
@@ -6,11 +6,13 @@ RSpec.feature 'Viewing service performance', type: :feature do
   scenario 'DfE users see service stats about user engagement' do
     given_there_have_been_sign_ins_from_responsible_body_and_mno_users
     and_some_bt_wifi_vouchers_have_been_downloaded
+    and_some_extra_mobile_data_requests_have_been_made
 
     when_i_sign_in_as_a_dfe_user
 
     then_i_see_stats_about_user_engagement
     and_i_see_stats_about_bt_wifi_vouchers
+    and_i_see_stats_about_extra_mobile_data_requests
   end
 
   def given_there_have_been_sign_ins_from_responsible_body_and_mno_users
@@ -20,6 +22,17 @@ RSpec.feature 'Viewing service performance', type: :feature do
 
   def and_some_bt_wifi_vouchers_have_been_downloaded
     create_list(:bt_wifi_voucher, 12, responsible_body: local_authority, distributed_at: 3.days.ago)
+  end
+
+  def and_some_extra_mobile_data_requests_have_been_made
+    create_list(:extra_mobile_data_request, 3,
+                status: :in_progress,
+                mobile_network: create(:mobile_network, brand: 'EE'),
+                created_by_user: create(:user, responsible_body: local_authority))
+    create_list(:extra_mobile_data_request, 5,
+                status: :complete,
+                mobile_network: create(:mobile_network, brand: 'Three'),
+                created_by_user: create(:user, responsible_body: local_authority))
   end
 
   def when_i_sign_in_as_a_dfe_user
@@ -34,5 +47,13 @@ RSpec.feature 'Viewing service performance', type: :feature do
 
   def and_i_see_stats_about_bt_wifi_vouchers
     expect(page).to have_text('12 vouchers downloaded by 1 responsible bodies')
+  end
+
+  def and_i_see_stats_about_extra_mobile_data_requests
+    expect(page).to have_text('8 requests')
+    expect(page).to have_text('3 in progress')
+    expect(page).to have_text('5 completed')
+    expect(page).to have_text('EE 3')
+    expect(page).to have_text('Three 5')
   end
 end
