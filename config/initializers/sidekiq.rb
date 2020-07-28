e9fcf9c8959ec631d4ce8f7e0a65862ef7729e1d
Al Davidson <apdavidson@gmail.com>
2020-07-28 10:38:10 +0100

    use rediss as protocol if ssl is enabled(==enforced)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
-
+redis_protocol == $redis_config[:ssl] ? 'rediss' : 'redis'
 if $redis_config[:password]
-  redis_url = "redis://:#{$redis_config[:password]}@#{$redis_config[:host]}:#{$redis_config[:port]}/0"
+  redis_url = "#{redis_protocol}://:#{$redis_config[:password]}@#{$redis_config[:host]}:#{$redis_config[:port]}/0"
 else
-  redis_url = "redis://#{$redis_config[:host]}:#{$redis_config[:port]}/0"
+  redis_url = "#{redis_protocol}://#{$redis_config[:host]}:#{$redis_config[:port]}/0"
 end
 
 Rails.logger.info "Configuring sidekiq with redis_url: #{redis_url}"
