cc3479aee3b74f1393864e4e360b7d5361bbf86d
Al Davidson <apdavidson@gmail.com>
2020-07-30 11:58:23 +0100

    Somehow these files didnt get added to #343 - maybe because of the rename?

---
+++ lib/v_cap_services_config.rb
@@ -0,0 +1,12 @@
+class VCapServicesConfig
+  attr_accessor :config
+
+  def initialize(config = ENV['VCAP_SERVICES'])
+    @config = JSON.parse(config)
+  end
+
+  def first_service_matching(name)
+    service_key = @config.keys.find { |svc| svc =~ /#{name}/i }
+    @config[service_key].first
+  end
+end
