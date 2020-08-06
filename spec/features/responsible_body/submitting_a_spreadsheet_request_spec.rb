cfd17299768be8bf33e75719d8168b60f0c11eac
Tony Headford <tony@binarycircus.com>
2020-08-06 15:24:13 +0100

    Added spec for submitting a spreadsheet

---
+++ spec/features/responsible_body/submitting_a_spreadsheet_request_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+require 'shared/filling_in_forms'
+
+RSpec.feature 'Submitting a bulk ExtraMobileDataRequest request', type: :feature do
+  context 'not signed in' do
+    it 'does not show the link in the nav' do
+      visit '/'
+      expect(page).not_to have_text('Tell us who needs more data')
+    end
+
+    scenario 'visiting the form directly should redirect to sign_in' do
+      visit new_responsible_body_mobile_bulk_request_path
+      expect(page).to have_current_path(sign_in_path)
+    end
+  end
+
+  context 'signed in' do
+    let(:user) { create(:local_authority_user) }
+    let(:mobile_network) { create(:mobile_network) }
+
+    before do
+      mobile_network
+      sign_in_as user
+      # prevent api call to Notify
+      ActiveJob::Base.queue_adapter = :test
+    end
+
+    after do
+      ActiveJob::Base.queue_adapter = :inline
+    end
+
+    scenario 'Navigating to the form' do
+      visit responsible_body_mobile_extra_data_requests_path
+      click_on('New request')
+      expect(page).to have_text('How would you like to submit information?')
+      choose('Using a spreadsheet')
+      click_on('Continue')
+      expect(page).to have_text('Pick a spreadsheet file')
+    end
+
+    scenario 'submitting the form without making a choice shows errors' do
+      visit new_responsible_body_mobile_bulk_request_path
+      click_on 'Upload requests'
+      expect(page.status_code).not_to eq(200)
+      expect(page).to have_text('There is a problem')
+    end
+
+    scenario 'submitting the form with a valid file shows a summary page' do
+      visit new_responsible_body_mobile_bulk_request_path
+      attach_file('Pick a spreadsheet file', file_fixture('extra-mobile-data-requests.xlsx'))
+      click_on 'Upload requests'
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_text('We’ve processed your spreadsheet')
+    end
+  end
+end
