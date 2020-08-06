e7d1e86a681c57504e6c9fe4fd04cfed46740dbf
Al Davidson <apdavidson@gmail.com>
2020-08-06 14:09:33 +0100

    fully-feature-specced workflow including both routes to setting the preferences and redirects back if appropriate

--- app/controllers/cookie_preferences_controller.rb
+++ app/controllers/cookie_preferences_controller.rb
@@ -5,14 +5,15 @@ class CookiePreferencesController < ApplicationController
 
   def create
     @form = CookiePreferencesForm.new(cookie_consent_params)
+
     if @form.valid?
       cookies["consented-to-cookies"] = {
-        value: user_preference,
+        value: cookie_consent_params[:cookie_consent],
         expires: Settings.cookie_consent.expiry_time_months.months.from_now
       }
 
       flash[:success] = I18n.t("cookie_preferences.success")
-      redirect_back(fallback_location: '/')
+      redirect_to( params[:return_url] || '/' )
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,6 +22,6 @@ class CookiePreferencesController < ApplicationController
 private
 
   def cookie_consent_params(opts = params)
-    params.fetch(:cookie_consent, {}).permit(:cookie_consent)
+    params.fetch(:cookie_preferences_form, {}).permit(:cookie_consent)
   end
 end
--- spec/features/cookie_preferences_behaviour_spec.rb
+++ spec/features/cookie_preferences_behaviour_spec.rb
@@ -1,44 +1,119 @@
 require 'rails_helper'
 
 RSpec.feature 'Cookie preferences', type: :feature do
-  context 'when no preference has been set' do
-    before do
-      set_cookie('consented-to-cookies', nil)
+  describe 'banner behaviour' do
+    context 'when no preference has been set' do
+      before do
+        set_cookie('consented-to-cookies', nil)
+      end
+
+      it 'shows the cookie banner on visiting the homepage' do
+        visit '/'
+        expect(page).to have_content('We use cookies')
+        expect(page).to have_link('set your cookie preferences', href: cookie_preferences_path)
+      end
+
+      it 'does not show the cookie banner on the cookie consent page' do
+        visit cookie_preferences_path
+        expect(page).not_to have_content('We use cookies')
+        expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+      end
+    end
+    context 'when a preference has been set to false' do
+      before do
+        set_cookie('consented-to-cookies', false)
+      end
+
+      it 'does not show the cookie banner on visiting the homepage' do
+        visit '/'
+        expect(page).not_to have_content('We use cookies')
+        expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+      end
     end
 
-    it 'shows the cookie banner on visiting the homepage' do
-      visit '/'
-      expect(page).to have_content('We use cookies')
-      expect(page).to have_link('set your cookie preferences', href: cookie_preferences_path)
-    end
+    context 'when a preference has been set to true' do
+      before do
+        set_cookie('consented-to-cookies', true)
+      end
 
-    it 'does not show the cookie banner on the cookie consent page' do
-      visit cookie_preferences_path
-      expect(page).not_to have_content('We use cookies')
-      expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+      it 'does not show the cookie banner on visiting the homepage' do
+        visit '/'
+        expect(page).not_to have_content('We use cookies')
+        expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+      end
     end
   end
-  context 'when a preference has been set to false' do
-    before do
-      set_cookie('consented-to-cookies', false)
-    end
 
-    it 'does not show the cookie banner on visiting the homepage' do
-      visit '/'
-      expect(page).not_to have_content('We use cookies')
-      expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
-    end
-  end
+  describe 'preferences page' do
+    context 'with no cookie existing' do
+      before do
+        set_cookie('consented-to-cookies', nil)
+      end
 
-  context 'when a preference has been set to true' do
-    before do
-      set_cookie('consented-to-cookies', true)
-    end
+      describe 'clicking on the Accept cookies button' do
+        let(:initial_page) { guide_to_collecting_mobile_information_path }
+        before do
+          visit initial_page
+          click_on 'Accept all cookies'
+        end
 
-    it 'does not show the cookie banner on visiting the homepage' do
-      visit '/'
-      expect(page).not_to have_content('We use cookies')
-      expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+        it 'redirects back to the initial page' do
+          expect(page.current_path).to eq(initial_page)
+        end
+
+        it 'tells me my preferences have been saved' do
+          expect(page).to have_content(I18n.t('cookie_preferences.success'))
+        end
+
+        it 'does not show the cookie banner' do
+          expect(page).not_to have_content('We use cookies')
+          expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+        end
+      end
+
+      describe 'clicking on the set preferences link' do
+        let(:initial_page) { '/' }
+        before do
+          visit initial_page
+          click_on 'set your cookie preferences'
+        end
+
+        it 'shows the preferences page' do
+          expect(page).to have_content('Do you accept Google Analytics cookies?')
+        end
+
+        describe 'when I click Save changes without selecting an option' do
+          before do
+            click_on 'Save changes'
+          end
+
+          it 'shows an error ' do
+            expect(page.current_path).to eq(cookie_preferences_path)
+            expect(page).to have_http_status(:unprocessable_entity)
+            expect(page).to have_content('Choose whether you consent to analytics cookies')
+          end
+        end
+
+        describe 'when I select an option and click Save changes' do
+          before do
+            choose('No, do not track my website usage')
+            click_on 'Save changes'
+          end
+
+          it 'redirects back to the home page' do
+            expect(page.current_path).to eq('/')
+          end
+
+          it 'tells me my preferences have been saved' do
+            expect(page).to have_content(I18n.t('cookie_preferences.success'))
+          end
+
+          it 'does not show the cookie banner' do
+            expect(page).not_to have_content('We use cookies')
+            expect(page).not_to have_link('set your cookie preferences', href: cookie_preferences_path)
+          end
+        end
+      end
     end
   end
 end
