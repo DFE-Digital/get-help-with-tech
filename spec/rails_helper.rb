2e090b79a99768bf4eabe2e107799dc551741692
Jake Benilov <benilov@gmail.com>
2020-08-08 23:00:02 +0100

    Run feature specs with sidekiq
    The other specs use the test ActiveJob adapter. This can be overridden by setting 'sidekiq: true|false'
    in the spec.
    
    This change allows to remove some repeated setup/teardowns in some specs.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'support/factory_bot'
 require 'support/controller_helper'
 require 'support/capybara_helper'
 require 'capybara/email/rspec'
+require 'support/sidekiq'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,4 +1,21 @@
+require 'sidekiq/testing'
 ActiveJob::Base.queue_adapter = :test
 
-require 'sidekiq/testing'
-Sidekiq::Testing.inline!
+RSpec.configure do |config|
+  include ActiveJob::TestHelper
+
+  # Turn Sidekiq on automatically in feature specs. Use `sidekiq: false` in
+  # tests to avoid Sidekiq running.
+  config.define_derived_metadata(file_path: Regexp.new('/spec/features/')) do |metadata|
+    metadata[:sidekiq] = true unless metadata[:sidekiq] == false
+  end
+
+  # Use `sidekiq: true` to run jobs immediately
+  config.around sidekiq: true do |example|
+    perform_enqueued_jobs do
+      Sidekiq::Testing.inline! do
+        example.run
+      end
+    end
+  end
+end
