077f0456a96f94cb0dc39a0d65812d559a87ce6b
Al Davidson <apdavidson@gmail.com>
2020-08-15 12:39:59 +0100

    make URN in schools factory 6 digits

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :school do
     association :responsible_body, factory: %i[local_authority trust].sample
-    urn { Faker::Number.number(digits: 5) }
+    urn { Faker::Number.number(digits: 6) }
     name { Faker::Educator.secondary_school }
     computacenter_reference { Faker::Number.number(digits: 8) }
   end
