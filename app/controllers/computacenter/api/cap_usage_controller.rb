6f34f47a40f26984b5a2bd9215c6e0b24aa7f8f2
Al Davidson <apdavidson@gmail.com>
2020-08-16 09:46:14 +0100

    create_batch => build_batch

--- app/controllers/computacenter/api/cap_usage_controller.rb
+++ app/controllers/computacenter/api/cap_usage_controller.rb
@@ -2,14 +2,14 @@ class Computacenter::API::CapUsageController < Computacenter::API::BaseControlle
   def bulk_update
     logger.info "bulk_update, params = #{params}"
     validate_xml!('CapUsage.xsd')
-    @batch = create_batch(@parsed_xml)
+    @batch = build_batch(@parsed_xml)
     @batch.process!
     render status: status_code(@batch)
   end
 
 private
 
-  def create_batch(parsed_xml)
+  def build_batch(parsed_xml)
     Computacenter::API::CapUsageUpdateBatch.new(parsed_xml['CapUsage'])
   end
 
