1b7638b24fa29dc53bee3f0ec4ca04dcf84610af
Jake Benilov <benilov@gmail.com>
2020-08-19 23:13:25 +0100

    Local authority importer should only import LAs that maintain schools

--- app/models/local_authorities_in_england_register.rb
+++ app/models/local_authorities_in_england_register.rb
@@ -3,12 +3,18 @@ require 'open-uri'
 class LocalAuthoritiesInEnglandRegister
   URL = 'https://local-authority-eng.register.gov.uk/records.json?page-size=5000'.freeze
 
-  def self.entries
+  # we believe that
+  # - combined authorities
+  # - strategic regional authorities and
+  # - non-metropolitan districts
+  # don't maintain schools
+  def self.local_authorities_that_maintain_schools
     local_authorities_register_results = JSON.parse(URI.open(URL).read)
 
     local_authorities_register_results
       .values
       .map { |result| result['item'].first }
       .reject { |result| result['end-date'].present? }
+      .reject { |result| result['local-authority-type'].in?(%w[COMB SRA NMD]) }
   end
 end
