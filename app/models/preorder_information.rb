483d14b5211af88f3e4e3a6846961c01f73897b5
Al Davidson <apdavidson@gmail.com>
2020-08-21 21:53:30 +0100

    add status to preorder_information

--- app/models/preorder_information.rb
+++ app/models/preorder_information.rb
@@ -1,3 +1,19 @@
 class PreorderInformation < ApplicationRecord
   self.table_name = 'preorder_information'
+
+  enum status: {
+    needs_contact: 'needs_contact',
+    needs_info: 'needs_info',
+    ready: 'ready',
+  }
+
+
+  # Update this method as we add more fields (e.g. chromebook info)
+  def infer_status
+    if who_will_order_devices == 'school'
+      :needs_contact
+    else
+      :needs_info
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_161806) do
+ActiveRecord::Schema.define(version: 2020_08_21_202014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,9 @@ ActiveRecord::Schema.define(version: 2020_08_21_161806) do
     t.string "who_will_order_devices", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["school_id"], name: "index_preorder_information_on_school_id"
+    t.index ["status"], name: "index_preorder_information_on_status"
   end
 
   create_table "responsible_bodies", force: :cascade do |t|
