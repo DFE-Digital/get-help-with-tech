714bb60616145af3c7d11961524b0f4b32e2e5ab
Jake Benilov <benilov@gmail.com>
2020-08-23 16:27:13 +0100

    School details summary list shows school contact if one is set

--- app/components/school_details_summary_list_component.rb
+++ app/components/school_details_summary_list_component.rb
@@ -1,6 +1,10 @@
 class SchoolDetailsSummaryListComponent < ViewComponent::Base
   validates :school, presence: true
 
+  delegate :school_will_order_devices?,
+           :school_contact,
+           to: :preorder_information
+
   def initialize(school:)
     @school = school
   end
@@ -23,6 +27,31 @@ class SchoolDetailsSummaryListComponent < ViewComponent::Base
         key: 'Who will order?',
         value: "The #{@school.preorder_information.who_will_order_devices_label.downcase} orders devices",
       },
-    ]
+    ] + school_contact_row_if_contact_present
+  end
+
+private
+
+  def school_contact_row_if_contact_present
+    if school_will_order_devices? && school_contact.present?
+      [{
+        key: 'School contact',
+        value: contact_lines.join('<br>'),
+      }]
+    else
+      []
+    end
+  end
+
+  def contact_lines
+    [
+      school_contact.title.present? ? "#{school_contact.title.upcase_first}: #{school_contact.full_name}" : school_contact.full_name,
+      school_contact.email_address,
+      school_contact.phone_number,
+    ].reject(&:blank?)
+  end
+
+  def preorder_information
+    @school.preorder_information
   end
 end
