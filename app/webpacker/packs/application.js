f0a573163e616e349af6c09d90e77c507ede5639
Paul Hayes <fofr@users.noreply.github.com>
2020-08-27 11:38:28 +0100

    Add JS for preventing leaving a page with unsaved changes
    Run JS on any form with a textarea or radio button

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,8 +2,10 @@ require.context('govuk-frontend/govuk/assets');
 
 import '../styles/application.scss';
 import { initAll } from 'govuk-frontend';
+import initWarnOnUnsavedChanges from "./warn-on-unsaved-changes";
 
 initAll();
+initWarnOnUnsavedChanges();
 
 require('jquery')
 require('./mno/extra-mobile-data-requests.js');
