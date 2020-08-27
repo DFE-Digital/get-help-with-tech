62075f5cce0b68474738796fd5aa940a67c1e0db
Al Davidson <apdavidson@gmail.com>
2020-08-27 10:34:45 +0100

    add specs for new RB methods, and add new fields to RB factories

--- app/models/responsible_body.rb
+++ app/models/responsible_body.rb
@@ -35,9 +35,11 @@ class ResponsibleBody < ApplicationRecord
 
   def self.find_by_computacenter_urn!(cc_urn)
     our_identifier = convert_computacenter_urn(cc_urn)
+    # given URNs starting with 't' are for Trusts
     if cc_urn.starts_with?('t')
       find_by_companies_house_number!(our_identifier)
     else
+      # If it's not a Trust, assume it's a school
       find_by_gias_id!(our_identifier)
     end
   end
@@ -52,7 +54,11 @@ class ResponsibleBody < ApplicationRecord
       # just remove the leading 'LEA'
       cc_urn[3..-1]
     else
+      # if we can't convert it, just return the given string
+      # - that way we can use it as-is in queries and it will raise
+      # an ActiveRecord::RecordNotFound error. If we'd returned nil, we'd
+      # get spurious matches
       cc_urn
-    end # return nil if we can't convert it (shou;d we raise?)
+    end
   end
 end
