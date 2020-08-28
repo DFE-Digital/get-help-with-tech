5d397926313c76e763be7fcfab9517e54a179b1c
Tony Headford <tony@binarycircus.com>
2020-08-28 19:07:09 +0100

    Added basic front-end and changes from comments

--- app/services/key_contacts_importer.rb
+++ app/services/key_contacts_importer.rb
@@ -6,7 +6,11 @@ class KeyContactsImporter
   end
 
   def import_contacts
+    summary = OpenStruct.new(success: 0, errors: [])
+    line = 1
+
     datasource.contacts do |contact_data|
+      line += 1
       rb = ResponsibleBody.find(contact_data[:id])
 
       contact = find_or_add_user!(rb, contact_data)
@@ -17,9 +21,12 @@ class KeyContactsImporter
 
       InviteResponsibleBodyUserMailer.with(user: contact,
                                            responsible_body: rb).nominate_contacts_email.deliver_later!
+      summary.success += 1
     rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound => e
+      summary.errors << "Line #{line}: #{e.message}"
       Rails.logger.error(e.message)
     end
+    summary
   end
 
   def self.import_from_url(url)
@@ -34,7 +41,7 @@ class KeyContactsImporter
 private
 
   def find_or_add_user!(rb, contact_data)
-    User.find_or_create_by(email_address: contact_data[:email_address]) do |u|
+    User.find_or_create_by!(email_address: contact_data[:email_address]) do |u|
       u.full_name = full_name(contact_data)
       u.telephone = contact_data[:telephone]
       u.responsible_body = rb
