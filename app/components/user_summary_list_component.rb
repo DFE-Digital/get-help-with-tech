22a1199c1957bc25d06e8a74c65a27fea67da263
Jake Benilov <benilov@gmail.com>
2020-08-31 23:19:37 +0100

    School users: display user attributes

---
+++ app/components/user_summary_list_component.rb
@@ -0,0 +1,24 @@
+class UserSummaryListComponent < ViewComponent::Base
+  validates :user, presence: true
+
+  def initialize(user:)
+    @user = user
+  end
+
+  def rows
+    [
+      {
+        key: 'Email address',
+        value: @user.email_address,
+      },
+      {
+        key: 'Telephone',
+        value: @user.telephone,
+      },
+      {
+        key: 'Last sign in',
+        value: @user.last_signed_in_at ? l(@user.last_signed_in_at, format: :short) : 'Never',
+      },
+    ]
+  end
+end
