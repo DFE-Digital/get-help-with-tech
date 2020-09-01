e7edd093a0d54c2c81bc9930fd74c55a268e8e27
Al Davidson <apdavidson@gmail.com>
2020-09-01 17:41:16 +0100

    remove contact & who will order rows

--- app/components/school/school_details_summary_list_component.rb
+++ app/components/school/school_details_summary_list_component.rb
@@ -23,33 +23,10 @@ class School::SchoolDetailsSummaryListComponent < ViewComponent::Base
         key: 'Type of school',
         value: @school.type_label,
       },
-      {
-        key: 'Who will order?',
-        value: "The #{(@school.preorder_information || @school.responsible_body).who_will_order_devices_label.downcase} orders devices",
-      },
-    ] + school_contact_row_if_contact_present + chromebook_rows_if_needed
+    ] + chromebook_rows_if_needed
   end
 
 private
-
-  def school_contact_row_if_contact_present
-    if school_will_order_devices? && school_contact.present?
-      [{
-        key: 'School contact',
-        value: contact_lines.map { |line| h(line) }.join('<br>').html_safe,
-      }]
-    else
-      []
-    end
-  end
-
-  def contact_lines
-    [
-      school_contact.title.present? ? "#{school_contact.title.upcase_first}: #{school_contact.full_name}" : school_contact.full_name,
-      school_contact.email_address,
-      school_contact.phone_number,
-    ].reject(&:blank?)
-  end
 
   def preorder_information
     @school.preorder_information
--- spec/components/school/school_details_summary_list_component_spec.rb
+++ spec/components/school/school_details_summary_list_component_spec.rb
@@ -17,10 +17,6 @@ describe School::SchoolDetailsSummaryListComponent do
       create(:school_device_allocation, school: school, device_type: 'std_device', allocation: 3)
     end
 
-    it 'confirms that fact' do
-      expect(result.css('dd')[3].text).to include('The school orders devices')
-    end
-
     it 'renders the school allocation' do
       expect(result.css('dd')[1].text).to include('3 devices')
     end
@@ -32,46 +28,10 @@ describe School::SchoolDetailsSummaryListComponent do
     it 'renders the school details' do
       expect(result.css('dd')[0].text).to include('Needs a contact')
     end
-
-    context 'and the headteacher has been set as the school contact' do
-      it 'displays the headteacher details' do
-        create(:preorder_information,
-               school: school,
-               who_will_order_devices: :school,
-               school_contact: headteacher)
-
-        expect(result.css('dt')[4].text).to include('School contact')
-        expect(result.css('dd')[4].inner_html).to include('Headteacher: Davy Jones<br>davy.jones@school.sch.uk<br>12345')
-      end
-    end
-
-    context 'and someone else has been set as the school contact' do
-      it "displays the new contact's details" do
-        new_contact = create(:school_contact, :contact,
-                             full_name: 'Jane Smith',
-                             email_address: 'abc@example.com',
-                             phone_number: '12345')
-        create(:preorder_information,
-               school: school,
-               who_will_order_devices: :school,
-               school_contact: new_contact)
-
-        expect(result.css('dt')[4].text).to include('School contact')
-        expect(result.css('dd')[4].text).to include('Jane Smith')
-        expect(result.css('dd')[4].text).to include('abc@example.com')
-        expect(result.css('dd')[4].text).to include('12345')
-      end
-    end
   end
 
   context 'when the responsible body will place device orders' do
     let(:school) { create(:school, :primary, :academy) }
-
-    it 'confirms that fact' do
-      create(:preorder_information, school: school, who_will_order_devices: :responsible_body)
-
-      expect(result.css('dd')[3].text).to include('The trust orders devices')
-    end
 
     it 'does not show the school contact even if the school contact is set' do
       create(:preorder_information,
