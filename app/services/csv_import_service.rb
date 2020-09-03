f4449cd98cab9978d619c7f08ea0de97aacf08b2
Al Davidson <apdavidson@gmail.com>
2020-09-03 15:41:21 +0100

    rubocop fix

--- app/services/csv_import_service.rb
+++ app/services/csv_import_service.rb
@@ -2,7 +2,7 @@ class CsvImportService
   def self.import!(csv_data_file)
     importer = CsvDataFileImporter.new(csv_data_file: csv_data_file)
     importer.import!
-    puts "#{importer.successes.count} successes, #{importer.failures.count} failures"
+    Rails.logger.info "#{importer.successes.count} successes, #{importer.failures.count} failures"
     { successes: importer.successes, failures: importer.failures }
   end
 end
