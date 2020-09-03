de1e6dbf179e4ddb7c75efc49707fdd98a3f5784
Al Davidson <apdavidson@gmail.com>
2020-09-03 16:28:54 +0100

    put CsvDataFile back to using .foreach, and download remote URIs to tempfiles if needed

--- app/models/csv_data_file.rb
+++ app/models/csv_data_file.rb
@@ -36,11 +36,25 @@ protected
 private
 
   def read_file
-    csv = URI.open(@csv_uri).read
-    CSV.parse(csv, headers: true, encoding: 'ISO8859-1:utf-8').select do |row|
+    csv = download_to_temp_file_if_needed(@csv_uri)
+    CSV.foreach(csv, headers: true, encoding: 'ISO8859-1:utf-8') do |row|
       next if skip?(row)
 
       yield row
     end
+  end
+
+  def download_to_temp_file_if_needed(uri)
+    is_remote?(uri) ? download_to_temp_file(uri) : uri
+  end
+
+  def is_remote?(uri)
+    %w[http https].include?(URI.parse(uri.to_s).scheme)
+  end
+
+  def download_to_temp_file(uri)
+    tempfile = Tempfile.new
+    RemoteFile.download(uri, tempfile)
+    tempfile.path
   end
 end
