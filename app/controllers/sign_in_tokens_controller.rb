0907d4bc23ec177a992013e0c41320bebf2d4910
Al Davidson <apdavidson@gmail.com>
2020-09-04 13:08:09 +0100

    rejig the sign-in token behaviour to only actually sign the user in once they click the button

--- app/controllers/sign_in_tokens_controller.rb
+++ app/controllers/sign_in_tokens_controller.rb
@@ -1,6 +1,4 @@
 class SignInTokensController < ApplicationController
-  before_action :require_sign_in!, only: :destroy
-
   def new
     @sign_in_token_form ||= SignInTokenForm.new
     if FeatureFlag.active?(:public_account_creation)
@@ -10,27 +8,25 @@ class SignInTokensController < ApplicationController
     end
   end
 
+  # GET /token/validate
+  # This does NOT sign the user in, but validates the token and renders a
+  # form with hidden fields that they must submit to actually sign-in
+  # We do it this way because we have found many users are hitting this page
+  # multiple times from different IPs, almost simultaneously - which implies
+  # that it's some automatic email link-scanning security software.
   def validate
-    @user = SessionService.validate_token!(token: params[:token], identifier: params[:identifier])
-    save_user_to_session!
-    EventNotificationsService.broadcast(SignInEvent.new(user: @user))
-    start_user_journey(@user)
-  rescue SessionService::TokenValidButExpired
-    # If it's the same user who already has a valid session, and they've just
-    # re-clicked a link with a token that's expired, but a session that _hasn't_
-    if SessionService.is_signed_in?(session) && @user.id == session[:user_id]
-      # - that's ok, we'll allow it
-      render :you_are_signed_in
-    else
-      render :token_is_valid_but_expired, status: :bad_request
+    validate_token_and do
+      render :click_to_sign_in
     end
-  rescue SessionService::TokenNotRecognised, SessionService::InvalidTokenAndIdentifierCombination
-    render :token_not_recognised, status: :bad_request
   end
 
   def destroy
-    @user.clear_token!
-    redirect_to root_url_for(@user)
+    validate_token_and do
+      save_user_to_session!
+      EventNotificationsService.broadcast(SignInEvent.new(user: @user))
+      @user.clear_token!
+      redirect_to root_url_for(@user)
+    end
   end
 
   def validate_manual
@@ -56,16 +52,6 @@ class SignInTokensController < ApplicationController
 
   def sent
     @user = User.where(sign_in_token: params[:token]).first
-    # NOTE: this is purely to allow us to display the link in the footer
-    # REMOVE when we have emails actually being sent
-    if @user
-      identifier = @user.sign_in_identifier(@user.sign_in_token)
-      @debug_info = {
-        sign_in_token: @user.sign_in_token,
-        sign_in_identifier: identifier,
-        sign_in_link: validate_sign_in_token_url(token: @user.sign_in_token, identifier: identifier),
-      }
-    end
   end
 
   def hide_nav_menu?
@@ -73,6 +59,24 @@ class SignInTokensController < ApplicationController
   end
 
 private
+
+  # if the token is valid, yield to the given block
+  # if not, handle appropriately
+  def validate_token_and
+    @user = SessionService.validate_token!(token: params[:token], identifier: params[:identifier])
+    yield
+  rescue SessionService::TokenValidButExpired
+    # If it's the same user who already has a valid session, and they've just
+    # re-clicked a link with a token that's expired, but a session that _hasn't_
+    if SessionService.is_signed_in?(session) && @user.id == session[:user_id]
+      # - that's ok, we'll allow it
+      yield
+    else
+      render :token_is_valid_but_expired, status: :bad_request
+    end
+  rescue SessionService::TokenNotRecognised, SessionService::InvalidTokenAndIdentifierCombination
+    render :token_not_recognised, status: :bad_request
+  end
 
   def sign_in_token_form_params
     params.require(:sign_in_token_form).permit(
@@ -83,14 +87,6 @@ private
     )
   end
 
-  def start_user_journey(user)
-    if user.is_school_user? && !user.school_welcome_wizard&.complete?
-      redirect_to school_welcome_wizard_welcome_path
-    else
-      render :you_are_signed_in
-    end
-  end
-
   def root_url_for(user)
     if user.is_mno_user?
       mno_extra_mobile_data_requests_path
@@ -99,13 +95,21 @@ private
     elsif user.is_responsible_body_user?
       responsible_body_home_path
     elsif user.is_school_user?
-      school_home_path
+      school_user_start_url(user)
     elsif user.is_computacenter?
       computacenter_home_path
     elsif user.is_support?
       support_internet_service_performance_path
     else
       '/'
+    end
+  end
+
+  def school_user_start_url(user)
+    if user.school_welcome_wizard&.complete?
+      school_home_path
+    else
+      school_welcome_wizard_welcome_path
     end
   end
 end
