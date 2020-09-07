8a23cfe823ea5cbbe48858e1e39abb7b72035131
Jake Benilov <benilov@gmail.com>
2020-09-07 22:12:41 +0100

    Make sure the navigate school welcome wizard spec works for all RBs
    The responsible body of the school that's used in the spec, is randomly
    a trust or a local authority. Parts of the user interface change depending
    on which one it is, which made the spec break in 50% of cases.

--- spec/features/school/navigate_school_welcome_wizard_spec.rb
+++ spec/features/school/navigate_school_welcome_wizard_spec.rb
@@ -166,7 +166,7 @@ RSpec.feature 'Navigate school welcome wizard' do
   def when_i_choose_yes_and_submit_the_chromebooks_form
     choose 'Yes, we will need Chromebooks'
     within('#school-welcome-wizard-will-need-chromebooks-yes-conditional') do
-      fill_in 'School or trust domain', with: 'example.com'
+      fill_in "School or #{school.responsible_body.humanized_type} domain", with: 'example.com'
       fill_in 'Recovery email address', with: 'admin@trust.com'
     end
     click_on 'Continue'
