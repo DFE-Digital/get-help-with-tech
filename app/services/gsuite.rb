d2ecbd31cc33e0379ee2fc421749ed7d54f23dc8
Tony Headford <tony@binarycircus.com>
2020-09-09 20:26:11 +0100

    Switched method of detection

--- app/services/gsuite.rb
+++ app/services/gsuite.rb
@@ -4,6 +4,10 @@ module Gsuite
   class DomainLookupError < StandardError; end
 
   def self.is_gsuite_domain?(domain, logger = nil)
+    has_service_login?(domain, logger)
+  end
+
+  def self.has_google_mx_record?(domain, logger = nil)
     response = HTTP.get("https://dns.google.com/resolve?name=#{domain}&type=MX")
     if response.status.success?
       payload = JSON.parse(response.body)
@@ -11,19 +15,20 @@ module Gsuite
 
       payload['Answer']&.any? { |h| h['data'] =~ /\.google(mail)?\.com/i }
     else
-      raise Gsuite::DomainLookupError.new("Domain lookup failed: #{response.status}")
+      raise DomainLookupError, "Domain lookup failed: #{response.status}"
     end
   end
 
   def self.has_service_login?(domain, logger = nil)
     response = HTTP.get("https://www.google.com/a/#{domain}/ServiceLogin")
     if response.status.success?
-      if response.body.to_s =~ /Sign in - Google Accounts/
+      body = response.body.to_s
+      logger.debug body if logger
+      if body =~ /Sign in - Google Accounts/
         true
-      elsif response.body.to_s =~ /Sorry, you've reached a login page for a domain that isn't using G Suite/
+      elsif body =~ /Sorry, you've reached a login page for a domain that isn't using G Suite/
         false
       else
-        logger.debug response.body.to_s if logger
         false
       end
     elsif response.status.redirect?
@@ -31,7 +36,7 @@ module Gsuite
       logger.debug response.body.to_s if logger
       true
     else
-      raise Gsuite::DomainLookupError.new("Service lookup failed: #{response.status}")
+      raise DomainLookupError, "Service lookup failed: #{response.status}"
     end
   end
 end
