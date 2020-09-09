e7556fe11f5f61e8dc1f6a24f885980dd755af7d
Tony Headford <tony@binarycircus.com>
2020-09-09 18:17:25 +0100

    Update regex and enable logging of DNS queries

--- app/services/gsuite.rb
+++ app/services/gsuite.rb
@@ -3,11 +3,13 @@ require 'http'
 module Gsuite
   class DomainLookupError < StandardError; end
 
-  def self.is_gsuite_domain?(domain)
+  def self.is_gsuite_domain?(domain, logger = nil)
     response = HTTP.get("https://dns.google.com/resolve?name=#{domain}&type=MX")
     if response.status.success?
       payload = JSON.parse(response.body)
-      payload['Answer']&.any? { |h| h['data'] =~ /\.google\.com/i }
+      logger.debug(payload) if logger
+
+      payload['Answer']&.any? { |h| h['data'] =~ /\.google(mail)?\.com/i }
     else
       raise Gsuite::DomainLookupError "Domain lookup failed: #{response.status}"
     end
