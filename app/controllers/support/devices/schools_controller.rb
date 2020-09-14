88929144967602f5fa16db8ef1cdcf7cd1b34136
Jake Benilov <benilov@gmail.com>
2020-09-14 16:29:13 +0100

    Handle case in support when inviting a school without a contact

--- app/controllers/support/devices/schools_controller.rb
+++ app/controllers/support/devices/schools_controller.rb
@@ -7,6 +7,10 @@ class Support::Devices::SchoolsController < Support::BaseController
   def confirm_invitation
     @school = School.find_by!(urn: params[:school_urn])
     @school_contact = @school.preorder_information&.school_contact
+    if @school_contact.nil?
+      flash[:warning] = I18n.t('support.schools.invite.no_school_contact', name: @school.name)
+      redirect_to support_devices_school_path(@school)
+    end
   end
 
   def invite
--- spec/controllers/support/devices/schools_controller_spec.rb
+++ spec/controllers/support/devices/schools_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Support::Devices::SchoolsController, type: :controller do
-  let(:school) { create(:school) }
+  let(:school) { create(:school, name: 'Alpha School') }
 
   describe 'show' do
     it 'is forbidden for MNO users' do
@@ -24,6 +24,22 @@ RSpec.describe Support::Devices::SchoolsController, type: :controller do
       get :show, params: { urn: school.urn }
 
       expect(response).to redirect_to(sign_in_path)
+    end
+  end
+
+  describe 'confirm_invitation' do
+    before do
+      create(:preorder_information, school: school, school_contact: nil)
+      sign_in_as create(:dfe_user)
+    end
+
+    context 'when the school has no school contact' do
+      it 'redirects back to the school page with an error' do
+        get :confirm_invitation, params: { school_urn: school.urn }
+
+        expect(response).to redirect_to(support_devices_school_path(school))
+        expect(request.flash[:warning]).to eq('Could not invite Alpha School because the school does not have a contact')
+      end
     end
   end
 end
