976391af915081d2f4f7603ec452612fd0fcd44d
Tony Headford <tony@binarycircus.com>
2020-09-17 14:49:48 +0100

    Add school order devices page

--- app/controllers/school/devices_controller.rb
+++ app/controllers/school/devices_controller.rb
@@ -4,8 +4,11 @@ class School::DevicesController < School::BaseController
     # branch of the user journey
     # if false && @school.can_order_devices?
     # else
-    render :cannot_order
-    # end
+    if @school.can_order_devices?
+      render :order
+    else
+      render :cannot_order
+    end
   end
 
   def request_devices
