26d8c1baf010a0b34b8f8b4a9e01f99d7cc120f8
Tony Headford <tony@binarycircus.com>
2020-09-17 20:51:09 +0100

    Download chromebook details as csv

---
+++ app/controllers/computacenter/chromebooks_controller.rb
@@ -0,0 +1,9 @@
+class Computacenter::ChromebooksController < Computacenter::BaseController
+  def index
+    respond_to do |format|
+      format.csv do
+        render csv: Computacenter::ChromebookDetails.export_to_csv, filename: "computacenter-chromebooks-#{Time.zone.now.iso8601}"
+      end
+    end
+  end
+end
---
+++ app/models/computacenter/chromebook_details.rb
@@ -0,0 +1,36 @@
+require 'csv'
+
+module Computacenter
+  class ChromebookDetails
+    def self.export_to_csv
+      CSV.generate do |csv|
+        csv << [
+          'Responsible Body URN',
+          'Responsible Body Name',
+          'School Name',
+          'School URN',
+          'Google Domain',
+          'Valid Recovery Off Domain Email Address',
+          'Date',
+          'Time'
+        ]
+        PreorderInformation
+          .includes(school: :responsible_body)
+          .where(will_need_chromebooks: 'yes')
+          .order(updated_at: :asc)
+          .each do |i|
+            csv << [
+              i.school.responsible_body.computacenter_identifier,
+              i.school.responsible_body.name,
+              i.school.name,
+              i.school.urn,
+              i.school_or_rb_domain,
+              i.recovery_email_address,
+              i.updated_at.utc.strftime('%d/%m/%Y'),
+              i.updated_at.utc.strftime('%R')
+            ]
+        end
+      end
+    end
+  end
+end
