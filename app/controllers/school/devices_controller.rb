d22b41d178b1dbf39b1e90fb6331ff743b7f97f5
Tony Headford <tony@binarycircus.com>
2020-09-17 17:06:38 +0100

    removed out-dated comments

--- app/controllers/school/devices_controller.rb
+++ app/controllers/school/devices_controller.rb
@@ -1,9 +1,5 @@
 class School::DevicesController < School::BaseController
   def order
-    # replace with if conditon below when we've developed the other
-    # branch of the user journey
-    # if false && @school.can_order_devices?
-    # else
     if @school.can_order_devices?
       render :order
     else
