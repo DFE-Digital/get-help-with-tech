8047ced983b739be1dfc20a88c7e7089d88f6588
Jake Benilov <benilov@gmail.com>
2020-09-20 15:34:50 +0100

    Invite the school user immediately when an RB user picks them as the contact

--- app/controllers/responsible_body/devices/who_to_contact_controller.rb
+++ app/controllers/responsible_body/devices/who_to_contact_controller.rb
@@ -16,7 +16,12 @@ class ResponsibleBody::Devices::WhoToContactController < ResponsibleBody::Device
       chosen_contact = @form.chosen_contact
       chosen_contact.save!
       @school.preorder_information.update!(school_contact: chosen_contact)
-      flash[:success] = I18n.t(:success, scope: %i[responsible_body devices schools who_to_contact create], email_address: chosen_contact.email_address)
+      is_success = @school.invite_school_contact
+      flash[:success] = I18n.t(
+        is_success ? :success : :failure,
+        scope: %i[responsible_body devices schools who_to_contact create],
+        email_address: chosen_contact.email_address,
+      )
       redirect_to responsible_body_devices_school_path(@school.urn)
     end
   end
@@ -38,7 +43,12 @@ class ResponsibleBody::Devices::WhoToContactController < ResponsibleBody::Device
       chosen_contact = @form.chosen_contact
       chosen_contact.save!
       @school.preorder_information.update!(school_contact: chosen_contact)
-      flash[:success] = I18n.t(:success, scope: %i[responsible_body devices schools who_to_contact create], email_address: chosen_contact.email_address)
+      is_success = @school.invite_school_contact
+      flash[:success] = I18n.t(
+        is_success ? :success : :failure,
+        scope: %i[responsible_body devices schools who_to_contact create],
+        email_address: chosen_contact.email_address,
+      )
       redirect_to responsible_body_devices_school_path(@school.urn)
     end
   end
