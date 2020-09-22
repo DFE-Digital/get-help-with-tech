9621a1d6c0537b1eafef6383b866ebe143c82e90
Tony Headford <tony@binarycircus.com>
2020-09-22 17:06:17 +0100

    Edit school users

--- config/routes.rb
+++ config/routes.rb
@@ -131,6 +131,7 @@ Rails.application.routes.draw do
       resources :key_contacts, only: %i[new index create], path: '/key-contacts'
       resources :responsible_bodies, only: %i[index show], path: '/responsible-bodies'
       resources :schools, only: %i[show], param: :urn do
+        resources :users, only: %i[edit update]
         resources :contacts, only: %i[edit update] do
           member do
             put :set_as_school_contact, path: 'set-as-school-contact'
