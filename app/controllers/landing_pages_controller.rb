42eb6e5738e504b744764046025a24f4922dcb3a
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-09-23 09:26:06 +0100

    spike into guidance page content

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,11 +1,30 @@
 class LandingPagesController < ApplicationController
+  layout 'page_with_toc', only: :get_support
+
   def get_laptops_and_tablets; end
 
-  def get_support; end
+  def get_support;
+    @title = I18n.t!('second_level_content.get_support.title')
+    render
+  end
 
   def get_internet_access; end
 
   def digital_platforms; end
 
   def edtech_demonstrator_programme; end
+
+private
+
+  def get_page_meta_data
+    I18n.t!('second_level_content').map do |page_id, page_metadata|
+      {
+        page_id: page_id,
+        # path: devices_guidance_subpage_path(subpage_slug: page_id.to_s.dasherize),
+        title: page_metadata[:title],
+        # description: page_metadata[:description],
+        # audience: page_metadata[:audience],
+      }
+    end
+  end
 end
