0458cc89d1b0783a60f8d83bc18160a154ce8cda
Phil Lee <asmega@gmail.com>
2020-09-24 16:50:47 +0100

    [733] Tweak ordering when specific circumstances
    - This change is only for devices

--- app/controllers/school/devices_controller.rb
+++ app/controllers/school/devices_controller.rb
@@ -1,7 +1,9 @@
 class School::DevicesController < School::BaseController
   def order
-    if @school.can_order_devices?
-      render :order
+    if @school.can_order? && @school.can_order_devices?
+      render :can_order
+    elsif @school.can_order_for_specific_circumstances? && @school.can_order_devices?
+      render :can_order_for_specific_circumstances
     else
       render :cannot_order
     end
