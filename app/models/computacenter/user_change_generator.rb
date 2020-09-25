06357a3bfd11dfaf6e87e318e30770a75c1f9d17
Al Davidson <apdavidson@gmail.com>
2020-09-25 09:00:48 +0100

    refactor generation code from user_change back into the generator

--- app/models/computacenter/user_change_generator.rb
+++ app/models/computacenter/user_change_generator.rb
@@ -1,194 +1,81 @@
 class Computacenter::UserChangeGenerator
-  attr_reader :version
+  attr_reader :user, :last_change_for_user
 
-  def initialize(version)
-    @version = version
+  def initialize(user)
+    @user = user
   end
 
-  def call
-    return if version.nil?
-
-    case type_of_update
-    when 'New'
-      return unless after_user.relevant_to_computacenter?
-    when 'Change'
-      return unless after_user.relevant_to_computacenter?
-    when 'Remove'
-      return unless before_user.relevant_to_computacenter?
+  def generate!
+    @last_change_for_user = Computacenter::UserChange.last_for(user)
+    if change_needed?
+      change = Computacenter::UserChange.new(consolidated_attributes)
+      change.add_original_fields_from(last_change_for_user) if last_change_for_user.present?
+      change.save!
+      change
     end
-
-    return if (version.changeset.keys & fields_to_monitor).empty?
-
-    Computacenter::UserChange.create!(user_change_attributes)
   end
 
 private
 
-  def strip_hybrid_fields!(hash)
-    if before_user.hybrid? || after_user.hybrid?
-      hash.delete(:school)
-      hash.delete(:school_urn)
-      hash.delete(:cc_ship_to_number)
-    end
+  def consolidated_attributes
+    computacenter_attributes\
+      .merge(meta_attributes)
+      .merge(type_of_update: type_of_update)
   end
 
-  def user_change_attributes
-    hash = meta_csv_attributes.merge(current_csv_fields).merge(original_csv_fields)
-    strip_hybrid_fields!(hash)
-    hash
+  def change_needed?
+    is_addition? || is_removal? || (is_change? && computacenter_fields_have_changed?)
   end
 
-  def current_csv_fields
+  def is_addition?
+    user.relevant_to_computacenter? && last_change_for_user.nil?
+  end
+
+  def is_change?
+    last_change_for_user.present? && \
+      user.relevant_to_computacenter? && \
+      !user.destroyed?
+  end
+
+  def is_removal?
+    (last_change_for_user.present? && !user.relevant_to_computacenter?) || \
+      (user.relevant_to_computacenter? && user.destroyed?)
+  end
+
+  def computacenter_fields_have_changed?
+    computacenter_attributes != last_change_for_user&.computacenter_attributes
+  end
+
+  def computacenter_attributes
     {
-      first_name: after_user.first_name,
-      last_name: after_user.last_name,
-      email_address: after_user.email_address,
-      telephone: after_user.telephone,
-      responsible_body: after_user.effective_responsible_body&.name,
-      responsible_body_urn: after_user.effective_responsible_body&.computacenter_identifier,
-      cc_sold_to_number: after_user.effective_responsible_body&.computacenter_reference,
-      school: after_user.school&.name,
-      school_urn: after_user.school&.urn,
-      cc_ship_to_number: after_user.school&.computacenter_reference,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email_address: user.email_address,
+      telephone: user.telephone,
+      responsible_body: user.effective_responsible_body&.name,
+      responsible_body_urn: user.effective_responsible_body&.computacenter_identifier,
+      cc_sold_to_number: user.effective_responsible_body&.computacenter_reference,
+      school: (user.hybrid? ? '' : user.school&.name),
+      school_urn: (user.hybrid? ? '' : user.school&.urn),
+      cc_ship_to_number: (user.hybrid? ? '' : user.school&.computacenter_reference),
     }
   end
 
-  def meta_csv_attributes
+  def meta_attributes
     {
-      user_id: version.item_id,
-      updated_at_timestamp: version.created_at,
+      user_id: user.id,
+      updated_at_timestamp: user.updated_at,
       type_of_update: type_of_update,
     }
   end
 
-  def fields_to_diff
-    %w[
-      full_name
-      telephone
-      email_address
-      responsible_body_id
-      school_id
-    ]
-  end
-
-  def fields_to_monitor
-    %w[
-      full_name
-      telephone
-      email_address
-      responsible_body_id
-      school_id
-      privacy_notice_seen_at
-      orders_devices
-    ]
-  end
-
-  def original_attributes
-    case type_of_update
-    when 'New'
-      {}
-    when 'Change'
-      original = version.changeset.transform_values(&:first).to_h
-      diffable_attributes_and_values(original)
-    when 'Remove'
-      original = version.object_deserialized
-      diffable_attributes_and_values(original)
-    end
-  end
-
-  def diffable_attributes_and_values(user_hash)
-    user_hash.filter { |k, _| fields_to_diff.include?(k) }.select { |_k, v| v }
-  end
-
-  def original_csv_fields
-    hash = original_attributes
-
-    expand_full_name_changes(hash)
-    expand_responsible_body_changes(hash)
-    expand_school_changes(hash)
-
-    hash.transform_keys! { |k| "original_#{k}" }
-
-    hash
-  end
-
-  def expand_full_name_changes(hash)
-    if hash.key?('full_name')
-      hash['first_name'] = before_user.first_name
-      hash['last_name'] = before_user.last_name
-
-      hash.delete('full_name')
-    end
-  end
-
-  def expand_responsible_body_changes(hash)
-    if hash.key?('responsible_body_id')
-      responsible_body = ResponsibleBody.find(hash['responsible_body_id'])
-
-      if responsible_body
-        hash['responsible_body'] = responsible_body.name
-        hash['responsible_body_urn'] = responsible_body.computacenter_identifier
-        hash['cc_sold_to_number'] = responsible_body.computacenter_reference
-      end
-
-      hash.delete('responsible_body_id')
-    end
-  end
-
-  def expand_school_changes(hash)
-    if hash.key?('school_id')
-      school = School.find(hash['school_id'])
-
-      if school
-        hash['school'] = school.name
-        hash['school_urn'] = school.urn
-        hash['cc_ship_to_number'] = school.computacenter_reference
-      end
-
-      hash.delete('school_id')
-    end
-  end
-
-  def before_user
-    @before_user ||= case version.event
-                     when 'create'
-                       User.new
-                     when 'update', 'destroy'
-                       User.new(version.object_deserialized)
-                     end
-  end
-
-  def after_user
-    @after_user ||= User.new(after_user_attributes)
-  end
-
-  def after_user_attributes
-    if version.event == 'create'
-      version.changeset.transform_values(&:last)
-    elsif version.event == 'destroy' || version.changeset['orders_devices'] == [true, false]
-      {}
-    elsif version.event == 'update'
-      version.object_deserialized.merge(version.changeset.transform_values(&:last))
-    end
-  end
-
   def type_of_update
-    if Computacenter::UserChange.where(user_id: (before_user.id || after_user.id)).exists?
-      if is_removal?
-        'Remove'
-      else
-        'Change'
-      end
+    if is_change?
+      'Change'
+    elsif is_removal?
+      'Remove'
     else
       'New'
     end
-  end
-
-  def is_removal?
-    version.event == 'destroy' || (
-      version.event == 'update' && \
-      before_user.relevant_to_computacenter? && \
-      !after_user.relevant_to_computacenter?
-    )
   end
 end
