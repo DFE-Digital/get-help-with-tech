297e2c05a6e63d6cad984b65718645cedf1a6f94
Tony Headford <tony@binarycircus.com>
2020-10-05 11:36:42 +0100

    trust updaet wip

---
+++ app/services/trust_update_service.rb
@@ -0,0 +1,38 @@
+class TrustUpdateService
+  def update_trusts
+    # look at the trusts that have changed since the last update
+    last_update = DataStage::DataUpdateRecord.last_update_for(:trusts)
+
+    # simple updates for trusts that are open
+    DataStage::Trust.updated_since(last_update).open.each do |staged_trust|
+      trust = Trust.find_by(urn: staged_trust.urn)
+      if trust
+        update_trust(trust, staged_trust)
+      else
+        create_trust(staged_trust)
+      end
+    end
+
+    DataStage::DataUpdateRecord.updated!(:trusts)
+  end
+
+private
+
+  def update_trust(trust, staged_trust)
+    # update trust details
+    attrs = staged_attributes(staged_trust)
+    trust.update!(attrs)
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error(e.record.errors)
+  end
+
+  def create_trust(staged_trust)
+    Trust.create!(staged_attributes(staged_trust))
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error(e.record.errors)
+  end
+
+  def staged_attributes(staged_trust)
+    staged_trust.attributes.except('id', 'created_at', 'updated_at')
+  end
+end
