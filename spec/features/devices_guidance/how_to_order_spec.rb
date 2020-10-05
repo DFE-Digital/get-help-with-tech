1966ce39950141ddef88521b458e416d33b42057
Jake Benilov <benilov@gmail.com>
2020-10-05 10:00:45 +0100

    Fix specs and formatting issues

--- spec/features/devices_guidance/how_to_order_spec.rb
+++ spec/features/devices_guidance/how_to_order_spec.rb
@@ -9,6 +9,6 @@ RSpec.feature 'How to order devices', type: :feature do
     expect(devices_guidance_how_to_order_page).to be_displayed
 
     expect(devices_guidance_how_to_order_page.page_heading).to have_content('How to order laptops and tablets during coronavirus (COVID-19)')
-    expect(devices_guidance_how_to_order_page.steps.length).to equal(5)
+    expect(devices_guidance_how_to_order_page.steps.length).to equal(6)
   end
 end
