98024837e29d17f9ad29c76e976212ef45e17df2
Jake Benilov <benilov@gmail.com>
2020-10-12 14:08:06 +0100

    Computacenter have requested extra info in the automated cap update email
    This email is sent to them after every cap change.

--- app/mailers/computacenter_mailer.rb
+++ app/mailers/computacenter_mailer.rb
@@ -31,6 +31,11 @@ private
       school_name: @school.name,
       urn: @school.urn,
       new_cap_value: @new_cap_value,
+      ship_to_number: @school.computacenter_reference,
+      responsible_body_name: @school.responsible_body.name,
+      responsible_body_type: @school.responsible_body.humanized_type,
+      responsible_body_reference: @school.responsible_body.computacenter_identifier,
+      sold_to_number: @school.responsible_body.computacenter_reference,
     }
   end
 
