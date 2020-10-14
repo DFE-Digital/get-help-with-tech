e7b1fd3d15442c6175bf49d8f4b1389f238260f3
Paul Hayes <fofr@users.noreply.github.com>
2020-10-14 11:59:01 +0100

    Navigate to reduced allocations pages in spec

--- spec/features/responsible_body/reduced_allocations_spec.rb
+++ spec/features/responsible_body/reduced_allocations_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature 'Reduced allocations due to supply chain delays' do
 
   scenario 'I see that allocation have been reduced' do
     given_i_am_signed_in_as_a_responsible_body_user
-    when_i_visit_the_reduced_allocations_page
+    when_i_follow_the_more_about_reduced_allocations_link
     then_i_see_that_allocations_were_reduced
   end
 
@@ -25,12 +25,12 @@ RSpec.feature 'Reduced allocations due to supply chain delays' do
     sign_in_as user
   end
 
-  def when_i_visit_the_reduced_allocations_page
-    visit responsible_body_devices_reduced_allocations_path
-    expect(page).to have_http_status(:ok)
+  def when_i_follow_the_more_about_reduced_allocations_link
+    click_on 'More about changes to allocations'
   end
 
   def then_i_see_that_allocations_were_reduced
+    expect(page).to have_http_status(:ok)
     expect(page).to have_text('We reduced device allocations because of delays in our supply chain')
     expect(page).to have_text('Allocations of devices are now based on')
   end
--- spec/features/school/reduced_allocation_spec.rb
+++ spec/features/school/reduced_allocation_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature 'Reduced allocation due to supply chain delays' do
 
   scenario 'when a school has an allocation' do
     given_i_am_signed_in_as_a_school_user
-    when_i_visit_the_reduced_allocation_page
+    when_i_follow_the_more_about_your_reduced_allocation_link
     then_i_see_my_reduced_allocation
   end
 
@@ -23,7 +23,7 @@ RSpec.feature 'Reduced allocation due to supply chain delays' do
 
     scenario 'I see that my allocation was removed' do
       given_i_am_signed_in_as_a_school_user
-      when_i_visit_the_reduced_allocation_page
+      when_i_follow_the_more_about_this_change_link
       then_i_see_that_my_allocation_was_removed
     end
   end
@@ -38,16 +38,21 @@ RSpec.feature 'Reduced allocation due to supply chain delays' do
     sign_in_as @school_user
   end
 
-  def when_i_visit_the_reduced_allocation_page
-    visit reduced_allocation_school_path(school)
-    expect(page).to have_http_status(:ok)
+  def when_i_follow_the_more_about_this_change_link
+    click_on 'More about this change to your allocation'
+  end
+
+  def when_i_follow_the_more_about_your_reduced_allocation_link
+    click_on 'More about your reduced allocation'
   end
 
   def then_i_see_my_reduced_allocation
+    expect(page).to have_http_status(:ok)
     expect(page).to have_text("Your new allocation of #{school.std_device_allocation.allocation}")
   end
 
   def then_i_see_that_my_allocation_was_removed
+    expect(page).to have_http_status(:ok)
     expect(page).to have_text('We removed your allocation because of delays in our supply chain')
   end
 end
