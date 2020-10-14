7c8e3077f73d27476388a1e84c0dcc7203a3f175
Jake Benilov <benilov@gmail.com>
2020-10-14 19:28:54 +0100

    Consolidate notifications logic into CanOrderDevicesNotifications
    This will make it easier to deliver consistent behaviour, irrespective
    what the trigger is for the notification.

--- app/models/can_order_devices_notifications.rb
+++ app/models/can_order_devices_notifications.rb
@@ -1,27 +1,34 @@
 class CanOrderDevicesNotifications
-  attr_reader :school
+  attr_reader :school, :user
 
-  def initialize(school:)
+  def initialize(school: nil, user: nil)
     @school = school
+    @user = user
   end
 
   def call
-    if school.can_order_devices_right_now?
-      if FeatureFlag.active?(:notify_can_place_orders)
-        status = school.preorder_information.status
-        if status.in? %w[needs_info school_contacted]
-          notify_all_organisation_users_that_action_is_needed
-          notify_computacenter
-        elsif status.in? %w[ready school_ready rb_can_order school_can_order]
-          notify_all_order_users_with_active_techsource_accounts
-          notify_computacenter
-        end
-      end
-      send_slack_notifications_for_users_who_can_order_right_now
+    if school&.can_order_devices_right_now?
+      notify_about_school_being_able_to_order
+    elsif user
+      notify_user_about_all_schools_they_can_order_for
     end
   end
 
 private
+
+  def notify_about_school_being_able_to_order
+    if FeatureFlag.active?(:notify_can_place_orders)
+      status = school.preorder_information.status
+      if status.in? %w[needs_info school_contacted]
+        notify_all_organisation_users_that_action_is_needed
+        notify_computacenter
+      elsif status.in? %w[ready school_ready rb_can_order school_can_order]
+        notify_all_order_users_with_active_techsource_accounts
+        notify_computacenter
+      end
+    end
+    send_slack_notifications_for_users_who_can_order_right_now
+  end
 
   def notify_all_order_users_with_active_techsource_accounts
     school.order_users_with_active_techsource_accounts.each do |user|
@@ -52,6 +59,15 @@ private
       .with(school: school, new_cap_value: new_cap_value)
       .notify_of_school_can_order
       .deliver_later
+  end
+
+  def notify_user_about_all_schools_they_can_order_for
+    user.schools_i_order_for.select(&:can_order_devices_right_now?).each do |school|
+      CanOrderDevicesMailer
+        .with(user: user, school: school)
+        .notify_user_email
+        .deliver_later
+    end
   end
 
   def new_cap_value
--- app/services/confirm_techsource_account_created_service.rb
+++ app/services/confirm_techsource_account_created_service.rb
@@ -40,12 +40,7 @@ private
     # Guard against multiple updates
     return if user.previous_changes.dig('techsource_account_confirmed_at', 0).present?
 
-    user.schools_i_order_for.select(&:can_order_devices_right_now?).each do |school|
-      CanOrderDevicesMailer
-        .with(user: user, school: school)
-        .notify_user_email
-        .deliver_later
-    end
+    CanOrderDevicesNotifications.new(user: user).call
   end
 
   def email_no_longer_computacenter_relevant(email)
