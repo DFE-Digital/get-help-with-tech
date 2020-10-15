7b906b8e2a801baa8e5afd77c2fa07fcf881711b
Jake Benilov <benilov@gmail.com>
2020-10-15 08:19:24 +0100

    Refactor CanOrderDevicesNotifications for clarity

--- app/models/can_order_devices_notifications.rb
+++ app/models/can_order_devices_notifications.rb
@@ -9,6 +9,7 @@ class CanOrderDevicesNotifications
   def call
     if school&.can_order_devices_right_now?
       notify_about_school_being_able_to_order
+      notify_computacenter
     elsif user
       notify_user_about_all_schools_they_can_order_for
     end
@@ -18,39 +19,45 @@ class CanOrderDevicesNotifications
 private
 
   def notify_about_school_being_able_to_order
+    message_type = what_message_to_send_about(school)
+
+    if message_type.present?
+      notify_users(
+        users: which_users_to_send_message_to(school: school, message_type: message_type),
+        school: school,
+        message_type: message_type,
+      )
+    end
+  end
+
+  def notify_user_about_all_schools_they_can_order_for
+    user.schools_i_order_for.select(&:can_order_devices_right_now?).each do |school|
+      notify_user(user: user, school: school, message_type: :user_can_order)
+    end
+  end
+
+  def what_message_to_send_about(school)
     case school.preorder_information&.status
     when nil, 'needs_contact'
       # TODO: we need to nudge the responsible body that this school needs a contact
     when 'school_will_be_contacted'
       # This is on the DfE to onboard these schools - there is nothing users can do in this case
     when 'needs_info', 'school_contacted'
-      notify_all_organisation_users_that_action_is_needed
-      notify_computacenter
+      :user_can_order_but_action_needed
     when 'ready', 'school_ready', 'rb_can_order', 'school_can_order'
-      notify_all_order_users_with_active_techsource_accounts
-      notify_computacenter
+      :user_can_order
     else
       raise "Unexpected preorder status #{school.preorder_information.status} for #{school.name} (#{school.urn})"
     end
   end
 
-  def notify_all_order_users_with_active_techsource_accounts
-    # TODO: what if there aren't any order users with active accounts?
-    school.order_users_with_active_techsource_accounts.each do |user|
-      notify_user_can_order(user: user, school: school)
-    end
-  end
-
-  def notify_all_organisation_users_that_action_is_needed
-    # TODO: what if there aren't any school organisation users?
-    school.organisation_users.each do |user|
-      notify_user_can_order_but_action_needed(user: user, school: school)
-    end
-  end
-
-  def notify_user_about_all_schools_they_can_order_for
-    user.schools_i_order_for.select(&:can_order_devices_right_now?).each do |school|
-      notify_user_can_order(user: user, school: school)
+  def which_users_to_send_message_to(school:, message_type:)
+    if message_type == :user_can_order
+      # TODO: what if there aren't any order users with active accounts?
+      school.order_users_with_active_techsource_accounts
+    elsif message_type == :user_can_order_but_action_needed
+      # TODO: what if there aren't any school organisation users?
+      school.organisation_users
     end
   end
 
@@ -63,26 +70,20 @@ private
     end
   end
 
-  def notify_user_can_order(user:, school:)
-    if FeatureFlag.active?(:notify_can_place_orders)
-      CanOrderDevicesMailer
-        .with(user: user, school: school)
-        .user_can_order
-        .deliver_later
-      EventNotificationsService.broadcast(
-        UserCanOrderEvent.new(user: user, school: school, type: :user_can_order),
-      )
+  def notify_users(users:, school:, message_type:)
+    users.each do |user|
+      notify_user(user: user, school: school, message_type: message_type)
     end
   end
 
-  def notify_user_can_order_but_action_needed(user:, school:)
+  def notify_user(user:, school:, message_type:)
     if FeatureFlag.active?(:notify_can_place_orders)
       CanOrderDevicesMailer
         .with(user: user, school: school)
-        .user_can_order_but_action_needed
+        .send(message_type)
         .deliver_later
       EventNotificationsService.broadcast(
-        UserCanOrderEvent.new(user: user, school: school, type: :user_can_order_but_action_needed),
+        UserCanOrderEvent.new(user: user, school: school, type: message_type),
       )
     end
   end
