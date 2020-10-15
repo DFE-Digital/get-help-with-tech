4140d68a2b47f88123589bcef7029b9156e0934c
Phil Lee <asmega@gmail.com>
2020-10-15 17:44:42 +0100

    [846] On TechSource confirm pick email to send

--- app/models/user_can_order_devices_notifications.rb
+++ app/models/user_can_order_devices_notifications.rb
@@ -13,23 +13,31 @@ private
 
   def notify_user_about_all_schools_they_can_order_for
     user.schools_i_order_for.select(&:can_order_devices_right_now?).each do |school|
-      notify_user(user: user, school: school)
+      notify_user(school: school)
     end
   end
 
-  def notify_user(user:, school:)
-    if FeatureFlag.active?(:notify_can_place_orders)
-      CanOrderDevicesMailer
-        .with(user: user, school: school)
-        .send(message_type)
-        .deliver_later
-      EventNotificationsService.broadcast(
-        UserCanOrderEvent.new(user: user, school: school, type: message_type),
-      )
-    end
+  def notify_user(school:)
+    return unless FeatureFlag.active?(:notify_can_place_orders)
+
+    message_type = message_type_for_school(school)
+
+    return unless message_type
+
+    CanOrderDevicesMailer
+      .with(user: user, school: school)
+      .send(message_type)
+      .deliver_later
+    EventNotificationsService.broadcast(
+      UserCanOrderEvent.new(user: user, school: school, type: message_type),
+    )
   end
 
-  def message_type
-    :user_can_order
+  def message_type_for_school(school)
+    if %w[rb_can_order school_can_order].include?(school.preorder_information&.status)
+      :user_can_order
+    elsif %w[needs_info].include?(school.preorder_information&.status)
+      :user_can_order_but_action_needed
+    end
   end
 end
---
+++ spec/models/user_can_order_devices_notifications_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe UserCanOrderDevicesNotifications do
+  subject(:service) { described_class.new(user: user) }
+
+  around do |example|
+    FeatureFlag.activate(:notify_can_place_orders)
+    example.run
+    FeatureFlag.deactivate(:notify_can_place_orders)
+  end
+
+  context 'when orders can be placed' do
+    let(:allocation) { create(:school_device_allocation, :with_std_allocation, :with_orderable_devices) }
+    let(:preorder) { create(:preorder_information, :school_will_order, status: :school_can_order) }
+    let(:school) { create(:school, preorder_information: preorder, std_device_allocation: allocation, order_state: :can_order) }
+    let(:user) { create(:school_user, orders_devices: true, school: school) }
+
+    it 'sends :user_can_order email' do
+      expect {
+        service.call
+      }.to have_enqueued_job.on_queue('mailers').with('CanOrderDevicesMailer', 'user_can_order', 'deliver_now', params: { user: user, school: school }, args: [])
+    end
+  end
+
+  context 'when orders cannot be placed' do
+    let(:allocation) { create(:school_device_allocation, :with_std_allocation, :with_orderable_devices) }
+    let(:preorder) { create(:preorder_information, :school_will_order, status: :needs_info) }
+    let(:school) { create(:school, preorder_information: preorder, std_device_allocation: allocation, order_state: :can_order) }
+    let(:user) { create(:school_user, orders_devices: true, school: school) }
+
+    it 'sends :user_can_order_but_action_needed email' do
+      expect {
+        service.call
+      }.to have_enqueued_job.on_queue('mailers').with('CanOrderDevicesMailer', 'user_can_order_but_action_needed', 'deliver_now', params: { user: user, school: school }, args: [])
+    end
+  end
+end
