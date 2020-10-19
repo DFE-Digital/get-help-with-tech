cd40e37a1058193d85f3c42fb2f2fe1199dc0e50
Phil Lee <asmega@gmail.com>
2020-10-19 16:05:07 +0100

    [793] Configurable text in DeviceCountComponent

--- app/components/device_count_component.rb
+++ app/components/device_count_component.rb
@@ -1,14 +1,20 @@
 class DeviceCountComponent < ViewComponent::Base
   include ViewHelper
 
-  attr_reader :max_count, :ordered_count, :action, :show_action, :show_availabilty
+  attr_reader :max_count, :ordered_count, :action, :show_action, :show_availabilty, :body_i18n_key
 
-  def initialize(max_count:, ordered_count:, show_action: true, action: {}, show_availabilty: true)
+  def initialize(max_count:,
+                 ordered_count:,
+                 show_action: true,
+                 action: {},
+                 show_availabilty: true,
+                 body_i18n_key: 'components.device_count_component.default_body')
     @max_count = max_count
     @ordered_count = ordered_count
     @action = action
     @show_action = show_action
     @show_availabilty = show_availabilty
+    @body_i18n_key = body_i18n_key
   end
 
   def show_availabilty?
