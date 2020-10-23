2df732147928459002ef81f29a464ea440b13dbd
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-10-23 13:33:40 +0100

    Adds a page layout for the second level pages
    The new second level pages will need to have breadcrumbs and Table of
    contents

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,6 @@
 class LandingPagesController < ApplicationController
+  layout 'page_with_toc'
+
   def edtech_demonstrator_programme
     @title = I18n.t!('landing_pages.edtech_demonstrator_programme.title')
     render
