fc31be0602acca47ca8d7da06cad35ac9189e66e
Al Davidson <apdavidson@gmail.com>
2020-10-26 10:52:47 +0000

    [876, part of 858] allow users with both an RB and at least one school to choose the organisation on login (#692)
    * allow existing Responsible Body users to be added to a school
    
    * allow users who have a ResponsibleBody and at least one School to choose their organisation on login
    
    * reflect "Your organisations" in breadcrumbs, and factor-out school_breadcrumbs into a Component
    
    * spec fixes

---
+++ app/components/school/school_breadcrumbs_component.rb
@@ -0,0 +1,47 @@
+class School::SchoolBreadcrumbsComponent < ViewComponent::Base
+  attr_accessor :user, :school, :items
+
+  def initialize(user:, school:, items:)
+    @user = user
+    @school = school
+    @items = items
+  end
+
+  def breadcrumbs(items)
+    items.map { |k, _v| k.is_a?(Hash) ? k : { k => '' } }.inject(:merge)
+  end
+
+  def scoped_breadcrumbs
+    breadcrumbs(scope + Array(items))
+  end
+
+  def scope
+    if user.responsible_body.present? && !user.is_a_single_academy_trust_user?
+      responsible_body_and_school_scope
+    elsif user.has_multiple_schools?
+      multiple_schools_scope
+    else
+      single_school_scope
+    end
+  end
+
+  def single_school_scope
+    [
+      { 'Home' => home_school_path(school) },
+    ]
+  end
+
+  def multiple_schools_scope
+    [
+      { 'Your schools' => schools_path },
+      { school.name => home_school_path(school) },
+    ]
+  end
+
+  def responsible_body_and_school_scope
+    [
+      { 'Your organisations' => schools_path },
+      { school.name => home_school_path(school) },
+    ]
+  end
+end
