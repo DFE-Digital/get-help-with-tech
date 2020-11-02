36d91f256ff6dbb855a6cea4bfc2d245a504b314
Al Davidson <apdavidson@gmail.com>
2020-11-02 10:06:58 +0000

    [908] Fix timing issue with async jobs (#728)
    * add a default_wait of 2s to all perform_later jobs & deliver_later mailer calls
    
    * replace all the wait: parameters with a custom ActiveJob queue adapter that does it all in one wrapper method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require 'active_job/queue_adapters/sidekiq_with_enforced_delay_adapter'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -56,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_adapter = :sidekiq_with_enforced_delay
   # config.active_job.queue_name_prefix = "govuk_rails_boilerplate_production"
 
   config.action_mailer.perform_caching = false
