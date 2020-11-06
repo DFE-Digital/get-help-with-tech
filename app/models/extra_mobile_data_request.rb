085f9ad8053e0783358cadf91ffd98e01fde89bc
Phil Lee <asmega@users.noreply.github.com>
2020-11-06 09:25:20 +0000

    Tweak scope to be more AR-like
    Co-authored-by: Jake Benilov <benilov@gmail.com>

--- app/models/extra_mobile_data_request.rb
+++ app/models/extra_mobile_data_request.rb
@@ -60,7 +60,7 @@ class ExtraMobileDataRequest < ApplicationRecord
   include ExportableAsCsv
 
   scope :from_responsible_bodies, -> { where.not(responsible_body: nil) }
-  scope :from_schools, -> { where('school_id IS NOT NULL') }
+  scope :from_schools, -> { where.not(school: nil) }
 
   def self.exportable_attributes
     {
