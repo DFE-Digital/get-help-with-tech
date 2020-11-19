a213acc6d7415ae969fe9389e861549ddc2988c1
Jake Benilov <benilov@gmail.com>
2020-11-19 12:01:56 +0000

    Make service performance in support only available to support users

--- app/controllers/support/service_performance_controller.rb
+++ app/controllers/support/service_performance_controller.rb
@@ -1,7 +1,8 @@
 class Support::ServicePerformanceController < Support::BaseController
-  before_action { authorize :support }
+  before_action { authorize Support::ServicePerformance }
 
   def index
+    skip_policy_scope
     @stats = Support::ServicePerformance.new
   end
 end
