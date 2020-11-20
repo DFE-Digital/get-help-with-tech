b4fb018000925913c77cbac58d47210ece2aac69
Phil Lee <asmega@gmail.com>
2020-11-20 11:44:25 +0000

    [1040] Add coverage for hiding closed schools

--- spec/controllers/support/responsible_bodies_controller_spec.rb
+++ spec/controllers/support/responsible_bodies_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Support::ResponsibleBodiesController, type: :controller do
-  describe 'index' do
+  describe '#index' do
     it 'is forbidden for MNO users' do
       sign_in_as create(:mno_user)
 
@@ -22,6 +22,24 @@ RSpec.describe Support::ResponsibleBodiesController, type: :controller do
       get :index
 
       expect(response).to redirect_to(sign_in_path)
+    end
+  end
+
+  describe '#show' do
+    let(:user) { create(:support_user) }
+    let!(:trust_user) { create(:trust_user) }
+    let!(:rb) { trust_user.responsible_body }
+    let!(:school) { create(:school, responsible_body: rb) }
+    let!(:closed_school) { create(:school, responsible_body: rb, status: :closed) }
+
+    before do
+      sign_in_as user
+    end
+
+    it 'excludes closed schools' do
+      get :show, params: { id: rb.id }
+      expect(assigns(:schools)).to include(school)
+      expect(assigns(:schools)).not_to include(closed_school)
     end
   end
 end
--- spec/form_objects/bulk_urn_search_form_spec.rb
+++ spec/form_objects/bulk_urn_search_form_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BulkUrnSearchForm do
   let(:school) { create(:school) }
+  let(:closed_school) { create(:school, status: :closed) }
 
   describe '#array_of_urns' do
     subject(:form) do
@@ -20,6 +21,17 @@ RSpec.describe BulkUrnSearchForm do
 
     it 'returns array of urns with no matches' do
       expect(form.missing_urns).to eql(%w[456])
+    end
+  end
+
+  describe '#schools' do
+    subject(:form) do
+      described_class.new(urns: "#{school.urn}\r\n#{closed_school.urn}\r\n")
+    end
+
+    it 'does not include closed schools' do
+      expect(form.schools).to include(school)
+      expect(form.schools).not_to include(closed_school)
     end
   end
 end
