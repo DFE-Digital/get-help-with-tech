6d276f21b7ef6f829645247fe81093ea5e538653
Al Davidson <apdavidson@gmail.com>
2020-12-01 13:33:00 +0000

    replace devices ordered panel with Order devices now link, when VCaps are enabled

--- spec/features/responsible_body/ordering_via_a_school_spec.rb
+++ spec/features/responsible_body/ordering_via_a_school_spec.rb
@@ -17,39 +17,83 @@ RSpec.feature 'Ordering via a school' do
     school_that_cannot_order_as_reopened.update!(preorder_information: another_preorder, std_device_allocation: another_allocation)
   end
 
-  context 'when school has no devices to order' do
-    scenario 'cannot order devices' do
+  context 'when the responsible body does not the vcap_feature_flag enabled' do
+    context 'when school has no devices to order' do
+      scenario 'cannot order devices' do
+        given_i_am_signed_in_as_rb_user
+
+        when_i_view_a_school(school)
+        then_i_see_status_of('Ready')
+        and_i_see_the_no_allocation_message
+      end
+    end
+
+    scenario 'when the school has reopened' do
       given_i_am_signed_in_as_rb_user
 
-      when_i_view_a_school(school)
-      then_i_see_status_of('Ready')
+      when_i_view_a_school(school_that_cannot_order_as_reopened)
+      then_i_see('You ordered 3 of 12 devices')
+    end
+
+    context 'when school has devices to order' do
+      let(:allocation) { create(:school_device_allocation, :with_std_allocation, :with_orderable_devices, cap: 12, devices_ordered: 3) }
+
+      before do
+        school.update!(std_device_allocation: allocation, order_state: 'can_order')
+        school.preorder_information.refresh_status!
+      end
+
+      scenario 'can order devices' do
+        given_i_am_signed_in_as_rb_user
+
+        when_i_view_a_school(school)
+        then_i_see_status_of('You can order')
+        and_i_see 'You’ve ordered 3 of 12 devices'
+
+        when_i_click_on('Order devices')
+        then_i_see_the_school_order_devices_page
+        and_i_see_the_techsource_button
+      end
     end
   end
 
-  scenario 'when the school has reopened' do
-    given_i_am_signed_in_as_rb_user
+  context 'when the virtual_caps feature flag is active and responsible body does have the vcap_feature_flag enabled', with_feature_flags: { virtual_caps: 'active' } do
+    let(:rb) { create(:trust, :vcap_feature_flag, schools: [school, school_that_cannot_order_as_reopened]) }
 
-    when_i_view_a_school(school_that_cannot_order_as_reopened)
-    then_i_see('You ordered 3 of 12 devices')
-  end
+    context 'when school has no devices to order' do
+      scenario 'cannot order devices' do
+        given_i_am_signed_in_as_rb_user
 
-  context 'when school has devices to order' do
-    let(:allocation) { create(:school_device_allocation, :with_std_allocation, :with_orderable_devices) }
-
-    before do
-      school.update!(std_device_allocation: allocation, order_state: 'can_order')
-      school.preorder_information.refresh_status!
+        when_i_view_a_school(school)
+        then_i_see_status_of('Ready')
+        and_i_do_not_see 'This school has no allocation'
+        and_i_do_not_see 'Order devices now'
+      end
     end
 
-    scenario 'can order devices' do
+    scenario 'when the school has reopened' do
       given_i_am_signed_in_as_rb_user
 
-      when_i_view_a_school(school)
-      then_i_see_status_of('You can order')
+      when_i_view_a_school(school_that_cannot_order_as_reopened)
+      and_i_do_not_see('You ordered 3 of 12 devices')
+      and_i_do_not_see 'Order devices now'
+    end
 
-      when_i_click_on('Order devices')
-      then_i_see_the_school_order_devices_page
-      and_i_see_the_techsource_button
+    context 'when the school can order devices and has an allocation' do
+      let(:allocation) { create(:school_device_allocation, :with_std_allocation, :with_orderable_devices, allocation: 12, cap: 10, devices_ordered: 3) }
+
+      before do
+        school.update!(std_device_allocation: allocation, order_state: 'can_order')
+        school.preorder_information.refresh_status!
+      end
+
+      scenario 'I do not see the number of devices' do
+        given_i_am_signed_in_as_rb_user
+
+        when_i_view_a_school(school)
+        then_i_do_not_see 'You’ve ordered 3 of 10 devices'
+        and_i_see_an_order_devices_now_link
+      end
     end
   end
 
@@ -79,5 +123,27 @@ RSpec.feature 'Ordering via a school' do
 
   def then_i_see(content)
     expect(page).to have_content(content)
+  end
+  alias_method :and_i_see, :then_i_see
+
+  def then_i_do_not_see(content)
+    expect(page).not_to have_content(content)
+  end
+  alias_method :and_i_do_not_see, :then_i_do_not_see
+
+  def and_i_do_not_see_an_order_devices_link
+    expect(page).not_to have_link('Order devices')
+  end
+
+  def and_i_see_that_all_devices_are_ordered
+    expect(page).to have_content('All devices ordered')
+  end
+
+  def and_i_see_the_no_allocation_message
+    expect(page).to have_content 'This school has no allocation'
+  end
+
+  def and_i_see_an_order_devices_now_link
+    expect(page).to have_link('Order devices now')
   end
 end
