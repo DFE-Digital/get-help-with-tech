3e383e07e7ba92230b1b1d848396faa6166d62d4
Tony Headford <tony@binarycircus.com>
2020-12-04 11:35:58 +0000

    updates to your schools page for rbs

--- app/controllers/responsible_body/devices/schools_controller.rb
+++ app/controllers/responsible_body/devices/schools_controller.rb
@@ -1,10 +1,14 @@
 class ResponsibleBody::Devices::SchoolsController < ResponsibleBody::BaseController
   def index
-    @schools = @responsible_body.schools
-                                .includes(:preorder_information)
-                                .includes(:std_device_allocation)
-                                .gias_status_open
-                                .order(name: :asc)
+    schools = @responsible_body.schools
+      .includes(:preorder_information)
+      .includes(:std_device_allocation)
+      .gias_status_open
+      .order(name: :asc)
+
+    @ordering_schools = schools.can_order
+    @specific_circumstances_schools = schools.can_order_for_specific_circumstances
+    @fully_open_schools = schools.where(order_state: %w[cannot_order cannot_order_as_reopened])
   end
 
   def show
