650a0f12c59953e7e375931b20a0bce2ae1e20ca
Phil Lee <asmega@gmail.com>
2020-12-07 14:27:37 +0000

    [1107] Email MN0s if new requests from last email

--- app/jobs/notify_mnos_job.rb
+++ app/jobs/notify_mnos_job.rb
@@ -5,12 +5,25 @@ class NotifyMnosJob < ApplicationJob
     MobileNetwork
       .joins(:extra_mobile_data_requests)
       .where(extra_mobile_data_requests: { status: %w[requested] })
+      .where('extra_mobile_data_requests.created_at > ?', since_last_email)
       .having('COUNT(mobile_networks.id) > 0')
       .group('mobile_networks.id').each do |mno|
-      number_of_new_requests = mno.extra_mobile_data_requests.requested.count
+      number_of_new_requests = mno.extra_mobile_data_requests.requested.where('created_at > ?', since_last_email).count
       mno.users.each do |user|
         MnoMailer.notify_new_requests(user: user, number_of_new_requests: number_of_new_requests).deliver_now
       end
+    end
+  end
+
+private
+
+  def since_last_email
+    now = Time.zone.now
+
+    if now.monday?
+      now - 72.hours
+    else
+      now - 24.hours
     end
   end
 end
--- spec/jobs/notify_mnos_job_spec.rb
+++ spec/jobs/notify_mnos_job_spec.rb
@@ -6,13 +6,20 @@ RSpec.describe NotifyMnosJob do
 
   let(:mno1) { create(:mobile_network, users: [mno_1_user]) }
   let(:mno2) { create(:mobile_network, users: [mno_2_user]) }
+  let(:now) { Time.zone.local(2020, 12, 7, 8) }
+
+  around do |example|
+    Timecop.freeze(now) do
+      example.run
+    end
+  end
 
   describe '#perform' do
     before do
-      create(:extra_mobile_data_request, mobile_network: mno1)
-      create(:extra_mobile_data_request, mobile_network: mno1)
-      create(:extra_mobile_data_request, mobile_network: mno1, status: :in_progress)
-      create(:extra_mobile_data_request, mobile_network: mno2, status: :in_progress)
+      create(:extra_mobile_data_request, mobile_network: mno1, created_at: 6.hours.ago) # in scope
+      create(:extra_mobile_data_request, mobile_network: mno1, created_at: 1.week.ago) # not in scope
+      create(:extra_mobile_data_request, mobile_network: mno1, created_at: 1.week.ago) # not in scope
+      create(:extra_mobile_data_request, mobile_network: mno2, status: :in_progress, created_at: 6.hours.ago) # not in scope
     end
 
     it 'emails relevant mno users' do
@@ -22,7 +29,7 @@ RSpec.describe NotifyMnosJob do
         described_class.perform_now
       }.to change { ActionMailer::Base.deliveries.size }.by(1)
 
-      expect(MnoMailer).to have_received(:notify_new_requests).with(user: mno_1_user, number_of_new_requests: 2)
+      expect(MnoMailer).to have_received(:notify_new_requests).with(user: mno_1_user, number_of_new_requests: 1)
     end
   end
 end
