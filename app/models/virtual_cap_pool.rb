ad8b539ca705bc59c020de9200f5b3445c07b028
Tony Headford <tony@binarycircus.com>
2020-12-09 11:27:42 +0000

    Prevent changes that don't affect cap or devices_ordered from generating cap updates

--- app/models/virtual_cap_pool.rb
+++ app/models/virtual_cap_pool.rb
@@ -17,11 +17,15 @@ class VirtualCapPool < ApplicationRecord
   end
 
   def recalculate_caps!
+    Rails.logger.info("***=== recalculating caps ===*** pool-id: #{id}")
+
     self.cap = school_device_allocations.sum(:cap)
     self.devices_ordered = school_device_allocations.sum(:devices_ordered)
     self.allocation = school_device_allocations.sum(:allocation)
+
+    notify_cc = cap_changed? || devices_ordered_changed?
     save!
-    notify_computacenter!
+    notify_computacenter! if notify_cc
   end
 
   def add_school!(school)
