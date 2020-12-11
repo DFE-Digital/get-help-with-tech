58fc2fec3553da5f6e2407ed05b17089562ad96b
Phil Lee <asmega@gmail.com>
2020-12-11 17:42:50 +0000

    [1107] NotifyMnos based on when last mail was sent

--- app/jobs/notify_mnos_job.rb
+++ app/jobs/notify_mnos_job.rb
@@ -2,14 +2,10 @@ class NotifyMnosJob < ApplicationJob
   queue_as :default
 
   def perform
-    MobileNetwork
-      .joins(:extra_mobile_data_requests)
-      .where(extra_mobile_data_requests: { status: %w[requested] })
-      .where('extra_mobile_data_requests.created_at > ?', since_last_email)
-      .having('COUNT(mobile_networks.id) > 0')
-      .group('mobile_networks.id').each do |mno|
-      number_of_new_requests = mno.extra_mobile_data_requests.requested.where('created_at > ?', since_last_email).count
-      mno.users.each do |user|
+    users.each do |user|
+      number_of_new_requests = user.mobile_network.extra_mobile_data_requests.requested.where('created_at > ?', since_last_email_for_user(user)).count
+
+      if number_of_new_requests.positive?
         MnoMailer.notify_new_requests(user: user, number_of_new_requests: number_of_new_requests).deliver_now
       end
     end
@@ -17,13 +13,11 @@ class NotifyMnosJob < ApplicationJob
 
 private
 
-  def since_last_email
-    now = Time.zone.now
+  def since_last_email_for_user(user)
+    user.email_audits.where(message_type: 'notify_new_requests').last&.created_at || 72.hours.ago
+  end
 
-    if now.monday?
-      now - 72.hours
-    else
-      now - 24.hours
-    end
+  def users
+    @users ||= User.joins(:mobile_network)
   end
 end
