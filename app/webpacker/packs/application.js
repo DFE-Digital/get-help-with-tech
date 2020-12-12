641284248fe0755d256e6c06c61d05cdc377cc14
Jake Benilov <benilov@gmail.com>
2020-12-12 15:07:07 +0000

    Add autocomplete styles

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,7 +1,9 @@
 require.context('govuk-frontend/govuk/assets');
 
-import '../styles/application.scss';
+import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
+
 import { initAll } from 'govuk-frontend';
+import '../styles/application.scss';
 import initWarnOnUnsavedChanges from "./warn-on-unsaved-changes";
 
 initAll();
