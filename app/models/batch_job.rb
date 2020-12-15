88377b7b6318bf2235776ae6dc6995cc4cf9855c
Al Davidson <apdavidson@gmail.com>
2020-12-15 16:18:24 +0000

    allow non-ActiveRecord objects like Hashes to be used for records:

--- app/models/batch_job.rb
+++ app/models/batch_job.rb
@@ -14,7 +14,7 @@ class BatchJob
     @successes = []
     @failures = []
     @records.each_with_index do |record, index|
-      log index: index, message: "processing #{record.class.name}: #{record.id}"
+      log index: index, message: "processing #{record.class.name}: #{record_id(record)}"
       yield record
       success!(record)
     rescue Exception => e
@@ -23,13 +23,21 @@ class BatchJob
     report_stats
   end
 
+  def report_stats
+    puts BatchJobLogEntry.status(run_id: @run_id)
+    puts BatchJobLogEntry.speed_stats(run_id: @run_id)
+  end
+
 private
+  def record_id(record)
+    record.respond_to?(:id) ? record.id : record.to_s.first(100)
+  end
 
   def success!(record)
     entry = BatchJobLogEntry.create!(
       job_name:     @job_name,
       run_id:       @run_id,
-      record_id:    record.id,
+      record_id:    record_id(record),
       record_class: record.class.name,
       status:       'success',
     )
@@ -37,12 +45,12 @@ private
   end
 
   def failure!(record:, error:)
-    BatchJobLogEntry.create!(
+    entry = BatchJobLogEntry.create!(
       job_name:     @job_name,
       run_id:       @run_id,
-      record_id:    record.id,
+      record_id:    record_id(record),
       record_class: record.class.name,
-      status:       'error',
+      status:       'failure',
       error:        error.message,
     )
     @failures << entry
