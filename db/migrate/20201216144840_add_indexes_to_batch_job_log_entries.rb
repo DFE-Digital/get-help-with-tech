1a8225f2e48b88128784bade9dfaa77b8e5afbee
Al Davidson <apdavidson@gmail.com>
2020-12-16 15:48:41 +0000

    rubocop fix

--- db/migrate/20201216144840_add_indexes_to_batch_job_log_entries.rb
+++ db/migrate/20201216144840_add_indexes_to_batch_job_log_entries.rb
@@ -1,7 +1,7 @@
 class AddIndexesToBatchJobLogEntries < ActiveRecord::Migration[6.0]
   def change
-    add_index :batch_job_log_entries, [:job_name, :created_at]
-    add_index :batch_job_log_entries, [:run_id, :created_at]
-    add_index :batch_job_log_entries, [:run_id, :record_class, :record_id], name: 'ix_btle_run_record'
+    add_index :batch_job_log_entries, %i[job_name created_at]
+    add_index :batch_job_log_entries, %i[run_id created_at]
+    add_index :batch_job_log_entries, %i[run_id record_class record_id], name: 'ix_btle_run_record'
   end
 end
