bbe44b11aa6a024515d7ac4c352db0b343844c03
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-12-18 22:01:53 +0000

    [286] Add zendesk integration

--- app/form_objects/support_ticket/check_your_request_form.rb
+++ app/form_objects/support_ticket/check_your_request_form.rb
@@ -6,7 +6,9 @@ class SupportTicket::CheckYourRequestForm
   validates :ticket, presence: true
 
   def create_ticket
-    ticket['subject'] = "TESTING - (#{ticket['school_unique_id']}) #{ticket['school_name']} "
-    ZendeskService.send!(ticket)
+    if Settings.zendesk.present? && Settings.zendesk.username.present? && Settings.zendesk.token.present?
+      ticket['subject'] = "TESTING - (#{ticket['school_unique_id']}) #{ticket['school_name']} "
+      ZendeskService.send!(ticket)
+    end
   end
 end
