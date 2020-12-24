846fb135b4e86c9a7f93df21e00cd0cd6909180c
Jake Benilov <benilov@gmail.com>
2020-12-24 17:17:11 +0000

    Support console: add an upper cap on the autosuggested school results
    Otherwise it's possible for a support agent to get thousands of results
    back if they accidentally enter a query string that will match lots of data,
    e.g. 'st' or a single letter search

--- app/form_objects/support/new_user_school_form.rb
+++ app/form_objects/support/new_user_school_form.rb
@@ -1,6 +1,8 @@
 class Support::NewUserSchoolForm
   include ActiveModel::Model
 
+  MAX_NUMBER_OF_SUGGESTED_SCHOOLS = 50
+
   attr_accessor :user, :name_or_urn
 
   def initialize(user:, name_or_urn: nil)
@@ -13,5 +15,6 @@ class Support::NewUserSchoolForm
       .matching_name_or_urn(@name_or_urn)
       .includes(:responsible_body)
       .order(:name)
+      .limit(MAX_NUMBER_OF_SUGGESTED_SCHOOLS)
   end
 end
