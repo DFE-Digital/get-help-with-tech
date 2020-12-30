017b883419f029d066784d2cfbb3f493b0d22c7a
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-12-30 15:16:43 +0000

    Reorder statement in application.js

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,15 +1,15 @@
 require.context('govuk-frontend/govuk/assets');
 
 import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
+import '../styles/application.scss';
 
 import { initAll } from 'govuk-frontend';
 import initResponsibleBodiesAutocomplete from "../scripts/responsible-bodies-autocomplete";
-import '../styles/application.scss';
 import initWarnOnUnsavedChanges from "../scripts/warn-on-unsaved-changes";
+import {initSelectAllNone} from '../scripts/mno/extra-mobile-data-requests'
 
 initAll();
 initWarnOnUnsavedChanges();
-import {initSelectAllNone} from '../scripts/mno/extra-mobile-data-requests'
 
 initSelectAllNone();
 initResponsibleBodiesAutocomplete();
