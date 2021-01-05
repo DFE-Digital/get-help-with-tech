cc17e725353ac7ebbaac58613b0f45b8e56d6c87
Phil Lee <asmega@gmail.com>
2021-01-05 12:21:36 +0000

    [1193] Show feature flags in support area

---
+++ spec/views/support/home/feature_flags.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'support/home/feature_flags.html.erb' do
+  let(:page) { PageObjects::Support::Home::FeatureFlagsPage.new }
+
+  it 'show feature flags statuses', with_feature_flags: { rate_limiting: 'active' } do
+    render
+    page.load(rendered)
+
+    expect(page.table['rate_limiting'].status).to eql('Active')
+    expect(page.table['slack_notifications'].status).to eql('Not active')
+  end
+end
