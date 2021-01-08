af969943068ef05345bc1bb6731877cbca07a4db
Al Davidson <apdavidson@gmail.com>
2021-01-08 17:15:50 +0000

    Revert "tell Cloudfront to cache all static assets for 1hr"
    This reverts commit 5ad3c74eb8662495681e5871a53bd5aa771787c2.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,6 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
