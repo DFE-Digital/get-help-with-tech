91f86d95a2af5eafdef93f4eb4b9dff7c41a73fa
Tony Headford <tony@binarycircus.com>
2021-01-11 15:29:03 +0000

    Use raw cap and allocation in validation

--- app/validators/cap_and_allocation_validator.rb
+++ app/validators/cap_and_allocation_validator.rb
@@ -6,8 +6,8 @@ class CapAndAllocationValidator < ActiveModel::Validator
 private
 
   def validate_cap_lte_allocation(record)
-    if record.cap.to_i > record.allocation.to_i
-      record.errors.add(:cap, :lte_allocation, allocation: record.allocation.to_i)
+    if record.raw_cap.to_i > record.raw_allocation.to_i
+      record.errors.add(:cap, :lte_allocation, allocation: record.raw_allocation.to_i)
     end
   end
 end
