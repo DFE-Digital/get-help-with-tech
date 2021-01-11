ceaf4c2f1c39562aa085569f7ca57a323a1062c6
Jake Benilov <benilov@gmail.com>
2021-01-11 23:12:09 +0000

    Extract out ExtraMobileDataRequestRow from ExtraMobileDataRequestSpreadsheet
    The row class is completely agnostic of Excel, meaning it can be re-used
    to implement CSV import.

--- app/models/extra_mobile_data_request_spreadsheet.rb
+++ app/models/extra_mobile_data_request_spreadsheet.rb
@@ -11,7 +11,12 @@ class ExtraMobileDataRequestSpreadsheet
     worksheet.map.with_index { |row, n|
       next if n.zero? || row.nil? # skip the header row
 
-      build_request(row)
+      row_hash = @headers.keys.inject({}) do |memo, column_sym|
+        column_index = @headers[column_sym]
+        memo[column_sym] = row[column_index]&.value
+        memo
+      end
+      ExtraMobileDataRequestRow.new(row_hash).build_request
     }.compact
   end
 
@@ -34,52 +39,5 @@ private
         @headers[column_heading.to_sym] = i
       end
     end
-  end
-
-  def build_request(row)
-    @current_row = row
-
-    request_attrs = fetch_request_attrs
-
-    ExtraMobileDataRequest.new(request_attrs) if request_attrs
-  end
-
-  def fetch_request_attrs
-    attrs = {
-      account_holder_name: account_holder_name,
-      device_phone_number: mobile_phone_number,
-      mobile_network_id: mobile_network_id,
-      contract_type: contract_type,
-      agrees_with_privacy_statement: agrees_with_privacy_statement,
-    }
-    return attrs unless attrs.values.all?(&:nil?)
-  end
-
-  def account_holder_name
-    column_value(:account_holder_name)
-  end
-
-  def mobile_phone_number
-    column_value(:mobile_phone_number)
-  end
-
-  def mobile_network_id
-    network_name = column_value(:mobile_network)
-    MobileNetwork.find_by(brand: network_name)&.id
-  end
-
-  def contract_type
-    contract_type = column_value(:pay_monthly_or_payg)
-    contract_type.parameterize.gsub('-', '_') if contract_type
-  end
-
-  def agrees_with_privacy_statement
-    column_value(:has_someone_shared_the_privacy_statement_with_the_account_holder)
-  end
-
-  def column_value(name)
-    column_name = name.to_sym
-    index = @headers[column_name]
-    @current_row[index]&.value
   end
 end
