39a3477c32b91e1cebb1a5f059b780663ab7a58f
Jake Benilov <benilov@gmail.com>
2021-01-11 23:49:28 +0000

    Refactor ExtraMobileDataRequestSpreadsheet to a more functional style

--- app/models/extra_mobile_data_request_spreadsheet.rb
+++ app/models/extra_mobile_data_request_spreadsheet.rb
@@ -6,21 +6,32 @@ class ExtraMobileDataRequestSpreadsheet
   end
 
   def requests
-    map_header_row
+    row_hashes.map do |row_hash|
+      ExtraMobileDataRequestRow.new(row_hash).build_request
+    end
+  end
+
+private
+
+  # Returns a collection of hashes mapping column name symbols to values:
+  #
+  # [
+  #   { account_holder_name: 'Jane Smith', mobile_phone_number: '07123456789', ... },
+  #   { account_holder_name: 'John Smith', mobile_phone_number: '07987654321', ... },
+  # ]
+  def row_hashes
+    header_row = worksheet.sheet_data.rows[0]
+    header_hash = header_name_to_column_mapping(header_row)
 
     worksheet.map.with_index { |row, n|
       next if n.zero? || row.nil? # skip the header row
 
-      row_hash = @headers.keys.inject({}) do |memo, column_sym|
-        column_index = @headers[column_sym]
+      header_hash.keys.each_with_object({}) do |column_sym, memo|
+        column_index = header_hash[column_sym]
         memo[column_sym] = row[column_index]&.value
-        memo
       end
-      ExtraMobileDataRequestRow.new(row_hash).build_request
     }.compact
   end
-
-private
 
   def worksheet
     @worksheet ||= extract_worksheet_from_spreadsheet
@@ -30,13 +41,14 @@ private
     RubyXL::Parser.parse(@spreadsheet_path)[WORKSHEET_NAME]
   end
 
-  def map_header_row
-    @headers = {}
-    row = worksheet.sheet_data.rows[0]
-    row.cells.each_with_index do |c, i|
-      if c&.value
-        column_heading = c.value.to_s.parameterize(separator: '_')
-        @headers[column_heading.to_sym] = i
+  # Returns a hash mapping the header column names (as symbols) to their column index
+  #
+  #   { account_holder_name: 0, mobile_phone_number: 1, ... },
+  def header_name_to_column_mapping(header_row)
+    header_row.cells.each_with_object({}) do |cell, memo|
+      if cell&.value
+        column_heading = cell.value.to_s.parameterize(separator: '_')
+        memo[column_heading.to_sym] = cell.column
       end
     end
   end
