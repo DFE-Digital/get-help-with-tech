9594d1aacf82a776dc4aa9c316f8d685774c509b
Paul Hayes <fofr@users.noreply.github.com>
2021-01-13 16:46:37 +0000

    Remove Jan 13 date, update to ASAP
    A few schools, recently added, cannot order yet because we are waiting on data from Computacenter.
    
    For these schools, tell them that ordering will be enabled ASAP.

--- spec/features/school/order_devices_spec.rb
+++ spec/features/school/order_devices_spec.rb
@@ -139,7 +139,7 @@ RSpec.feature 'Order devices' do
   end
 
   def then_i_see_that_i_will_be_able_to_order_soon
-    expect(page).to have_content('We’re opening orders for primary schools gradually, and will invite all to order by 13 January.')
+    expect(page).to have_content('Ordering will be opened for your school as soon as possible')
   end
 
   def then_i_see_techsource_ready_soon
