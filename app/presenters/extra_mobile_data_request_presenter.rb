ab240ff8a78a3565d18c5327050f9837a926dea8
Jake Benilov <benilov@gmail.com>
2021-01-15 00:22:21 +0000

    Remove bespoke error handling in ExtraMobileDataRequestPresenter
    This logic duplicates the error messages from ExtraMobileDataRequest
    and prevents an unexpected message from appearing.

--- app/presenters/extra_mobile_data_request_presenter.rb
+++ app/presenters/extra_mobile_data_request_presenter.rb
@@ -19,26 +19,7 @@ class ExtraMobileDataRequestPresenter < SimpleDelegator
   end
 
   def error_message
-    return unless errors.any?
-
-    msgs = []
-    errors.messages.each_key do |k|
-      case k
-      when :device_phone_number
-        msgs << if device_phone_number.present?
-                  'Not a mobile number'
-                else
-                  'No mobile number provided'
-                end
-      when :mobile_network_id
-        msgs << 'Not a known network'
-      when :account_holder_name
-        msgs << 'No account holder provided'
-      when :agrees_with_privacy_statement
-        msgs << 'Privacy statement not shared with account holder'
-      end
-    end
-    msgs.join('<br/>').html_safe
+    errors.messages.values.join('<br>').html_safe
   end
 
   def contract_type_options
