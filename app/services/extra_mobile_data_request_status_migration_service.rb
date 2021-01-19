e69052881b1675a834d1092824376cde6016699c
Jake Benilov <benilov@gmail.com>
2021-01-19 14:45:12 +0000

    Revert "Service class to migrate queried MNO requests to problem statuses"
    This reverts commit 6007d0e57130ffcfd764f09bbfe98fe26b5f6339.
    
    The service class has been run on production and is no longer needed.

--- app/services/extra_mobile_data_request_status_migration_service.rb
+++
@@ -1,9 +0,0 @@
-class ExtraMobileDataRequestStatusMigrationService
-  def call
-    ExtraMobileDataRequest
-      .queried
-      .find_each do |request|
-        request.update!(status: "problem_#{request.problem}")
-      end
-  end
-end
