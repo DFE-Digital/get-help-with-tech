e6e9907457c7a3360f6184f23941b64c85147a86
Jake Benilov <benilov@gmail.com>
2021-01-22 14:55:52 +0000

    Explain MNO request statuses to responsible body users

--- app/controllers/responsible_body/internet/mobile/extra_data_requests_controller.rb
+++ app/controllers/responsible_body/internet/mobile/extra_data_requests_controller.rb
@@ -3,6 +3,7 @@ class ResponsibleBody::Internet::Mobile::ExtraDataRequestsController < Responsib
 
   def index
     @pagination, @extra_mobile_data_requests = pagy(@responsible_body.extra_mobile_data_requests.order(:created_at))
+    @statuses_with_descriptions = statuses_with_descriptions
   end
 
   def guidance; end
@@ -35,5 +36,19 @@ private
     params.fetch(:extra_mobile_data_submission_form, {}).permit(%i[
       submission_type
     ])
+  end
+
+  def statuses_with_descriptions
+    ExtraMobileDataRequest
+      .statuses_that_school_and_rb_users_can_see
+      .collect do |status|
+        [
+          status,
+          I18n.t!(
+            "#{status}.school_or_rb_user_description",
+            scope: %i[activerecord attributes extra_mobile_data_request status],
+          ),
+        ]
+      end
   end
 end
