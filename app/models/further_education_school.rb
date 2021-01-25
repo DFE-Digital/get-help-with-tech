d668b9862390e94ffb78c2e1ab6de911f585511f
Phil Lee <asmega@gmail.com>
2021-01-25 14:55:58 +0000

    add FurtherEducationSchool#type_label

--- app/models/further_education_school.rb
+++ app/models/further_education_school.rb
@@ -17,4 +17,29 @@ class FurtherEducationSchool < School
       'organisation'
     end
   end
+
+  def type_label
+    case fe_type
+    when 'general_fe_and_tertiary'
+      'General FE and Tertiary'
+    when 'sixth_form_college'
+      'Sixth Form College'
+    when 'independent_learning_provider'
+      'Independent Learning Provider'
+    when 'agricultural_and_horticultural_college'
+      'Agricultural & Horticultural College'
+    when 'special_post_16_institution'
+      'Post-16 Institution (SPI)'
+    when '16_19_other'
+      '16-19 other'
+    when 'local_authority'
+      'Local Authority'
+    when 'art_and_design_college'
+      'Art and Design College'
+    when 'higher_education_provider'
+      'Higher Education Provider'
+    else
+      'Other'
+    end
+  end
 end
