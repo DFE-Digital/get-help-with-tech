2c53480d098e19ea52ed0ab965babbdb7aa14b66
Al Davidson <apdavidson@gmail.com>
2021-02-08 13:12:31 +0000

    Update all unavailable requests to new, where the MNO is participating

---
+++ db/migrate/20210208113843_set_all_unavailable_emdrs_to_new_for_participating_networks.rb
@@ -0,0 +1,16 @@
+class SetAllUnavailableEmdrsToNewForParticipatingNetworks < ActiveRecord::Migration[6.1]
+  def up
+    reqs = ExtraMobileDataRequest.joins(:mobile_network)
+                                 .where(status: 'unavailable')
+                                 .where(mobile_network: {participation_in_pilot: 'participating'})
+    # we're updating each one individually rather than in bulk,
+    # so that we get a PaperTrail record for each
+    reqs.each do |req|
+      req.update(status: 'new')
+    end
+  end
+
+  def down
+    # no-op
+  end
+end
