864fbc499ff47bf1afe4c55ed9eb82c872d4682e
Alistair Laing <aliuk2012@users.noreply.github.com>
2021-02-10 16:31:20 +0000

    Fix deprecation warning, adding errors to model
    DEPRECATION WARNING: Calling `<<` to an ActiveModel::Errors message
    array in order to add an error is deprecated.
    Please call `ActiveModel::Errors#add` instead.

--- app/validators/gsuite_domain_validator.rb
+++ app/validators/gsuite_domain_validator.rb
@@ -1,7 +1,9 @@
 class GsuiteDomainValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.blank? || !Gsuite.is_gsuite_domain?(value)
-      record.errors[attribute] << (options[:message] || 'is not a valid G Suite domain')
+
+      record.errors.add(attribute, message: (options[:message] || 'is not a valid G Suite domain'))
+      # record.errors[attribute] << (options[:message] || 'is not a valid G Suite domain')
     end
   end
 end
