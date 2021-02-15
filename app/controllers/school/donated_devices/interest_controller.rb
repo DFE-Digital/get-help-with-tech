c9a229cafaa7f2c9264d1c2c9cf9dd8e7bed5a65
Tony Headford <tony@binarycircus.com>
2021-02-15 11:58:19 +0000

    Adding device selection

--- app/controllers/school/donated_devices/interest_controller.rb
+++ app/controllers/school/donated_devices/interest_controller.rb
@@ -26,7 +26,7 @@ class School::DonatedDevices::InterestController < School::BaseController
     if request.post?
       if @form.valid?
         if @form.interested?
-          # TODO: Redirect to 'select devices'
+          redirect_to what_devices_do_you_want_donated_devices_school_path(@school)
         else
           render :not_interested
         end
@@ -36,9 +36,35 @@ class School::DonatedDevices::InterestController < School::BaseController
     end
   end
 
+  def device_types
+    @form = DonatedDeviceSelectionForm.new(device_types_params.merge(state: :select_devices))
+    if request.post?
+      if @form.valid?
+        redirect_to how_many_devices_donated_devices_school_path(@school)
+      else
+        render :device_types, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def how_many_devices
+    @form = DonatedDeviceSelectionForm.new(device_types_params.merge(state: :select_units))
+    if request.post?
+      if @form.valid?
+        # redirect_to how_many_devices_donated_devices_school_path(@school)
+      else
+        render :how_many_devices, status: :unprocessable_entity
+      end
+    end
+  end
+
 private
 
   def device_interest_params(opts = params)
     opts.fetch(:donated_device_interest_form, {}).permit(:device_interest)
+  end
+
+  def device_types_params(opts = params)
+    opts.fetch(:donated_device_selection_form, {}).permit(:units, device_types: [])
   end
 end
--- app/models/donated_device_request.rb
+++ app/models/donated_device_request.rb
@@ -1,9 +1,8 @@
 class DonatedDeviceRequest < ApplicationRecord
   DEVICE_TYPES = %w[
-    windows-laptop
-    windows-tablet
-    android-tablet
+    windows
     chromebook
+    android-tablet
     ipad
   ].freeze
 
@@ -11,11 +10,14 @@ class DonatedDeviceRequest < ApplicationRecord
   belongs_to :school
 
   validates :units, :device_types, presence: true
+  validates :device_types, presence: { message: 'Tell us which devices you want' }
   validate :validate_applicable_device_types
 
 private
 
   def validate_applicable_device_types
-    errors.add(:device_types, 'includes an invalid device type') unless device_types.all? { |d| DEVICE_TYPES.include?(d) }
+    if device_types
+      errors.add(:device_types, 'includes an invalid device type') unless device_types.all? { |d| DEVICE_TYPES.include?(d) }
+    end
   end
 end
