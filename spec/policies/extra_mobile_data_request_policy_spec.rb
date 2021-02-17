e1c7a0f692725e040624362c37c92f2a341fbfad
Al Davidson <apdavidson@gmail.com>
2021-02-17 14:12:15 +0000

    add policy to restrict access to extra_mobile_data_requests in support

---
+++ spec/policies/extra_mobile_data_request_policy_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe ExtraMobileDataRequestPolicy do
+  subject(:policy) { described_class }
+  let(:object) { build(:extra_mobile_data_request) }
+
+  permissions :new?, :create?, :edit?, :update?, :destroy? do
+    it 'blocks access to support users' do
+      expect(policy).not_to permit(build(:support_user), object)
+    end
+
+    it 'blocks access to Computacenter users' do
+      expect(policy).not_to permit(build(:computacenter_user), object)
+    end
+  end
+
+  permissions :index?, :show? do
+    it 'grants access to support users' do
+      expect(policy).to permit(build(:support_user), object)
+    end
+
+    it 'blocks access to Computacenter users' do
+      expect(policy).not_to permit(build(:computacenter_user), object)
+    end
+  end
+end
