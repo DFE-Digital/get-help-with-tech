e94a035ba1bf4780f15fb34e115c25687c6abe68
Al Davidson <apdavidson@gmail.com>
2021-02-19 15:08:30 +0000

    add giffgiff to the participating mobile networkds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_133454) do
+ActiveRecord::Schema.define(version: 2021_02_19_145448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -215,8 +215,8 @@ ActiveRecord::Schema.define(version: 2021_02_17_133454) do
     t.string "county"
     t.string "postcode"
     t.string "status", default: "open", null: false
-    t.string "computacenter_change", default: "none", null: false
     t.boolean "vcap_feature_flag", default: false
+    t.string "computacenter_change", default: "none", null: false
     t.boolean "new_fe_wave", default: false
     t.index ["computacenter_change"], name: "index_responsible_bodies_on_computacenter_change"
     t.index ["computacenter_reference"], name: "index_responsible_bodies_on_computacenter_reference"
@@ -308,6 +308,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_133454) do
     t.string "type", default: "CompulsorySchool", null: false
     t.integer "ukprn"
     t.text "fe_type"
+    t.boolean "hide_mno", default: false
     t.boolean "group_a_feature_flag", default: false, null: false
     t.boolean "hide_mno", default: false
     t.index ["computacenter_change"], name: "index_schools_on_computacenter_change"
@@ -403,8 +404,8 @@ ActiveRecord::Schema.define(version: 2021_02_17_133454) do
     t.boolean "orders_devices"
     t.datetime "techsource_account_confirmed_at"
     t.datetime "deleted_at"
-    t.boolean "rb_level_access", default: false, null: false
     t.text "role", default: "no", null: false
+    t.boolean "rb_level_access", default: false, null: false
     t.index "lower((email_address)::text)", name: "index_users_on_lower_email_address_unique", unique: true
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
