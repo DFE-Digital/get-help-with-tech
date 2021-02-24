674788825c1005531b92e894870ced9fd1ebdd53
Tony Headford <tony@binarycircus.com>
2021-02-24 15:26:01 +0000

    Spike - adding LA funding places school

--- app/models/local_authority.rb
+++ app/models/local_authority.rb
@@ -15,4 +15,30 @@ class LocalAuthority < ResponsibleBody
   }
 
   validates :organisation_type, presence: true
+
+  def la_funded_places
+    schools.la_funded_places_establishment_type.first
+  end
+
+  def create_la_funded_places!(urn:, device_allocation: 0, router_allocation: 0, extra_args: {})
+    return unless la_funded_places.nil?
+    attrs = {
+      responsible_body: self,
+      urn: urn,
+      name: 'LA Funded Places',
+      establishment_type: 'la_funded_places',
+      address_1: address_1,
+      address_2: address_2,
+      address_3: address_3,
+      town: town,
+      county: county,
+      postcode: postcode,
+    }.reverse_merge(extra_args)
+
+    funded_places = LaFundedPlace.create!(attrs)
+    funded_places.create_preorder_information!(who_will_order_devices: 'responsible_body', will_need_chromebooks: 'no')
+    funded_places.device_allocations.std_device.create!(allocation: device_allocation)
+    funded_places.device_allocations.coms_device.create!(allocation: router_allocation)
+    funded_places
+  end
 end
