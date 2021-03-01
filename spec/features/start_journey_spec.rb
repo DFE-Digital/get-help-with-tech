338b837cb8be29e8d0fda4ab538fe403b6519a65
Alistair Laing <aliuk2012@users.noreply.github.com>
2021-03-01 11:30:45 +0000

    Add missing page titles to two second level pages

--- spec/features/start_journey_spec.rb
+++ spec/features/start_journey_spec.rb
@@ -10,12 +10,14 @@ RSpec.feature 'View pages', type: :feature do
     when_i_visit_the_home_page
     and_i_click_on_free_training_and_free_support_link
     then_i_should_see_the_edtech_programme_page
+    and_the_page_title_should_be_set(@edtech_landing_page, 'Get free training and support to set up and use technology effectively')
   end
 
   scenario 'user would like to find out hot to get a digital platform setup' do
     when_i_visit_the_home_page
     and_i_click_on_get_funding_to_setup_platforms_link
     then_i_should_see_the_digital_platforms_page
+    and_the_page_title_should_be_set(@digital_platforms_page, 'Get funding and support to set up a digital education platform')
   end
 
 private
@@ -45,5 +47,10 @@ private
   def then_i_should_see_the_digital_platforms_page
     @digital_platforms_page ||= PageObjects::LandingPages::DigitalPlatformsPage.new
     expect(@digital_platforms_page).to be_displayed
+  end
+
+  def and_the_page_title_should_be_set(current_page, title)
+    expect(current_page)
+      .to have_title "#{title} - Get help with technology - GOV.UK"
   end
 end
