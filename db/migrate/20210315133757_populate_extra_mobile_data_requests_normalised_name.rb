ba0d9476c3a63384b6ed076cb6d7c3777b6c3da3
Al Davidson <apdavidson@gmail.com>
2021-03-16 12:40:18 +0000

    remove body of migration that blocks releases

--- db/migrate/20210315133757_populate_extra_mobile_data_requests_normalised_name.rb
+++ db/migrate/20210315133757_populate_extra_mobile_data_requests_normalised_name.rb
@@ -1,7 +1,6 @@
 class PopulateExtraMobileDataRequestsNormalisedName < ActiveRecord::Migration[6.1]
   def change
-    ExtraMobileDataRequest.find_each do |record|
-      record.update!(normalised_name: record.normalise_name)
-    end
+    # make this migration a no-op as it times out in production. I'll
+    # just have to populate the field from the console
   end
 end
