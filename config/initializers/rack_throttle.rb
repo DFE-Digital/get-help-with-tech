daee74243f7a83ccd7b80a82324f8d765ccaca5a
Al Davidson <apdavidson@gmail.com>
2021-03-19 12:21:07 +0000

    allow rack-throttle to use Redis & update sign-in paths

--- config/initializers/rack_throttle.rb
+++ config/initializers/rack_throttle.rb
@@ -11,9 +11,20 @@ class RackThrottleConfig
     { method: 'DELETE', limit: Integer(Settings.throttling.limits.delete) },
     { method: 'GET', limit: Integer(Settings.throttling.limits.get) },
     { method: 'GET', path: '/token/.*', limit: Integer(Settings.throttling.limits['/token'].get) },
-    { method: 'GET', path: '/sign_in_tokens/.*', limit: Integer(Settings.throttling.limits['/sign_in_tokens'].get) },
+    { method: 'GET', path: '/sign-in/.*', limit: Integer(Settings.throttling.limits['/sign-in'].get) },
+    { method: 'POST', path: '/sign-in/.*', limit: Integer(Settings.throttling.limits['/sign-in'].post) },
   ].freeze
   DEFAULT = Settings.throttling.default_limit
+
+  def self.redis_url
+    if ENV['REDIS_URL'].present?
+      ENV['REDIS_URL']
+    elsif ENV['VCAP_SERVICES'].present?
+      require 'v_cap_services_config'
+      redis_config = VCapServicesConfig.new.first_service_matching('redis')
+      redis_config['credentials']['uri']
+    end
+  end
 end
 
 # only do this if enabled, so that we don't throttle features specs, for instance
@@ -23,5 +34,7 @@ if FeatureFlag.active?(:rate_limiting)
                                           default: RackThrottleConfig::DEFAULT,
                                           code: 429,
                                           message: File.read(Rails.root.join('public/429.html')),
-                                          type: 'text/html; charset=utf-8'
+                                          type: 'text/html; charset=utf-8',
+                                          cache: RackThrottleConfig.redis_url.present? ? Redis.new(url: RackThrottleConfig.redis_url) : {},
+                                          key_prefix: :throttle
 end
