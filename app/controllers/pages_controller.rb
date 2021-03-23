ff8e996ffd531156c976ce3512c1982becc971d3
Alistair Laing <aliuk2012@users.noreply.github.com>
2021-03-23 10:48:13 +0000

    Remove obsolete controller method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,11 @@
 class PagesController < ApplicationController
-  before_action :show_parent_carer_pupil_banner?, except: %i[accessibility privacy start]
+  before_action :show_parent_carer_pupil_banner?, except: %i[accessibility privacy]
 
   layout 'single_page', only: %i[accessibility privacy]
 
   def guidance; end
 
   def increasing_mobile_data_privacy_notice; end
-
-  def start
-    if SessionService.is_signed_in?(session) && @current_user
-      redirect_to root_url_for(@current_user)
-    end
-  end
 
   def home_page
     @show_parent_carer_pupil_banner = false
