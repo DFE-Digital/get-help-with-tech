a512253809dfe64bde74fa5d5030c7e984c3a803
Phil Lee <asmega@gmail.com>
2021-03-30 13:19:20 +0100

    [1820] Configure environments for sentry

--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -2,4 +2,5 @@ Sentry.init do |config|
   config.dsn = Settings.sentry.dsn
   config.release = ENV['GIT_COMMIT_SHA']
   config.traces_sample_rate = 0.2
+  config.environment = ENV['SERVICE_ENV'] || 'development'
 end
