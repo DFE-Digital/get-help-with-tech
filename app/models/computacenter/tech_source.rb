254e426bdb8830c166695148e604f4fab27b6363
Mark Allen <markhallen@gmail.com>
2021-04-01 16:18:04 +0200

    Add maintenance banner for first weekend of April 2021

--- app/models/computacenter/tech_source.rb
+++ app/models/computacenter/tech_source.rb
@@ -1,9 +1,9 @@
 class Computacenter::TechSource
   NEXT_MAINTENANCE = {
-    window_start: Time.zone.local(2020, 12, 12, 9, 0, 0),
-    window_end: Time.zone.local(2020, 12, 12, 17, 0, 0),
-    maintenance_on_date: Date.new(2020, 12, 12),
-    reopened_on_date: Date.new(2020, 12, 13),
+    window_start: Time.zone.local(2021, 4, 3, 9, 0, 0),
+    window_end: Time.zone.local(2021, 4, 3, 12, 0, 0),
+    maintenance_on_date: Date.new(2021, 4, 3),
+    reopened_on_date: Date.new(2021, 4, 4),
   }.freeze
 
   def next_maintenance
