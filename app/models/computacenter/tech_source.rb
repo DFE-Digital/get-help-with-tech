e29602cb9bcf6a69699f180d824530ec3c9f0f26
Alistair Laing <aliuk2012@users.noreply.github.com>
2021-04-08 13:28:55 +0100

    Update the next TechSource maintenance window

--- app/models/computacenter/tech_source.rb
+++ app/models/computacenter/tech_source.rb
@@ -1,5 +1,5 @@
 class Computacenter::TechSource
-  MAINTENANCE_WINDOW = (Time.zone.parse('3 Apr 2021 09:00')..Time.zone.parse('3 Apr 2021 12:00')).freeze
+  MAINTENANCE_WINDOW = (Time.zone.parse('10 Apr 2021 09:00')..Time.zone.parse('10 Apr 2021 13:00')).freeze
 
   def url
     Settings.computacenter.techsource_url
