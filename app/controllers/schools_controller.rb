18d2f6596e965cd2b1258068318cd0500ab13383
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-04-09 16:00:55 +0100

    Updated text on 'Your Organisations' page to be less terse

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -4,7 +4,7 @@ class SchoolsController < ApplicationController
   def index
     @schools = impersonated_or_current_user.schools
 
-    if impersonated_or_current_user.schools.size == 1 && \
+    if impersonated_or_current_user.schools.one? && \
         (impersonated_or_current_user.is_a_single_school_user? || !impersonated_or_current_user.is_responsible_body_user?)
       redirect_to home_school_path(impersonated_or_current_user.schools.first)
     end
