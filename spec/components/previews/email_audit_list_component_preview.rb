c9d9294b472c151ee3fe0aa0157d093d838fbd10
Alistair Laing <aliuk2012@users.noreply.github.com>
2021-04-13 14:47:04 +0100

    Remove FactoryBot from email audit preview
    Generally speaking component previews should be static examples and shouldn't
    care how the data is structured. In this commit, I've replaced FactoryBot
    with mocked out data set. Previews that use FactoryBot will not work
    if the app is running in Producation mode which is what our review apps
    and GOV.UK Paas dev instance is doing. This is because FactoryBot gem
    is only installed for development & test.

--- spec/components/previews/email_audit_list_component_preview.rb
+++ spec/components/previews/email_audit_list_component_preview.rb
@@ -1,6 +1,25 @@
 class EmailAuditListComponentPreview < ViewComponent::Preview
   def default
-    audits = FactoryBot.build_list(:email_audit, 3, created_at: 3.days.ago)
-    render(Support::EmailAuditListComponent.new(audits))
+    render(Support::EmailAuditListComponent.new(mock_audit))
+  end
+
+private
+
+  def mock_audit
+    [OpenStruct.new({ template: SecureRandom.uuid,
+                      created_at: Time.zone.now,
+                      user: OpenStruct.new({ full_name: 'Ken Block' }),
+                      email_address: 'ken.block@example.com',
+                      message_type: 'user_can_order_but_action_needed' }),
+     OpenStruct.new({ template: SecureRandom.uuid,
+                      created_at: Time.zone.now - 5.minutes,
+                      user: OpenStruct.new({ full_name: 'Ken Block' }),
+                      email_address: 'ken.block@example.com',
+                      message_type: 'user_can_order' }),
+     OpenStruct.new({ template: SecureRandom.uuid,
+                      created_at: Time.zone.now - 15.minutes,
+                      user: OpenStruct.new({ full_name: 'Sally Block' }),
+                      email_address: 'ken.block@example.com',
+                      message_type: 'nudge_rb_to_add_school_contact' })]
   end
 end
