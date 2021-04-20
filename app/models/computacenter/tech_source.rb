f1b23e132edc8bfc9344f348340c8f69823b3469
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-04-20 11:07:44 +0100

    Add next TechSource maintenance window

--- app/models/computacenter/tech_source.rb
+++ app/models/computacenter/tech_source.rb
@@ -1,5 +1,5 @@
 class Computacenter::TechSource
-  MAINTENANCE_WINDOW = (Time.zone.parse('10 Apr 2021 09:00')..Time.zone.parse('10 Apr 2021 13:00')).freeze
+  MAINTENANCE_WINDOW = (Time.zone.parse('23 Apr 2021 6:00pm')..Time.zone.parse('25 Apr 2021 8:00pm')).freeze
 
   def url
     Settings.computacenter.techsource_url
