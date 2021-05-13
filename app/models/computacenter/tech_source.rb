430671cb9b06e1e6b35eabd443a742bf6225ed49
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-05-13 16:40:25 +0100

    Change outage window (current one abandoned)

--- app/models/computacenter/tech_source.rb
+++ app/models/computacenter/tech_source.rb
@@ -1,5 +1,5 @@
 class Computacenter::TechSource
-  MAINTENANCE_WINDOW = (Time.zone.parse('15 May 2021 8:00am')..Time.zone.parse('15 May 2021 10:00pm')).freeze
+  MAINTENANCE_WINDOW = (Time.zone.parse('29 May 2021 09:00am')..Time.zone.parse('29 May 2021 12:00pm')).freeze
 
   def url
     Settings.computacenter.techsource_url
