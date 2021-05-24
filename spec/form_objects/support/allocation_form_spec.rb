9a62760ad0d8ed4ade04bccba09f0b9914b9df09
Jits <dev@jits.co.uk>
2021-05-24 17:18:29 +0100

    Tactical fix to allow allocations to be decreased
    … for schools that are not in a virtual cap pool.
    
    Note: I've gone for a "light touch" fix here that doesn't attempt to change too much – there's a longer term piece required for refactoring + code consolidation + potential fixes, later on, after we figure out / confirm what the intended product design for allocations and vcap pools is moving forward.
    
    Changes here:
    
    - Allow allocations to be decreased, but only for schools that are not in a virtual cap pool.
    - For schools that are in a virtual cap pool, we a) show a warning message in the UI that decreasing is not allowed, and b) have a validation (at the form model layer) to prevent decreasing.
    - The allocation now has a minimum, set to the current `SchoolDeviceAllocation#raw_devices_ordered`, via a validation in the form model. The UI has also been updated to show a hint.
      - Note: this validation is only on the form model layer as we only want to enforce it for the support user flow. See <https://trello.com/c/FxejJUlY/716-remove-the-validation-that-devicesordered-cannot-be-greater-than-cap> for context on why this is not a model validation.
    - Updated existing specs + new specs for the `AllocationForm` model.
    
    Note an edge case here: because the main change here mirrors the cap adjustment logic in `SchoolAllocationDevice#cap_implied_by_order_state`, the cap is not adjusted for schools that have an order state of `can_order_for_specific_circumstances`, which could result in the same HTTP 422 error bug hitting if an allocation is decreased below the cap for a school with this order state.

---
+++ spec/form_objects/support/allocation_form_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+RSpec.describe Support::AllocationForm do
+  describe 'validations' do
+    context 'when in a virtual cap pool' do
+      let(:responsible_body) { create(:trust, :manages_centrally, :vcap_feature_flag) }
+      let :school do
+        create(:school, :with_preorder_information, :in_lockdown, responsible_body: responsible_body)
+      end
+      let! :school_device_allocation do
+        create(:school_device_allocation, school: school, allocation: initial_value)
+      end
+
+      subject :form do
+        described_class.new(allocation: updated_value, school_allocation: school_device_allocation)
+      end
+
+      before do
+        # Properly associate things with a virtual cap pool.
+        school.preorder_information.responsible_body_will_order_devices!
+        responsible_body.add_school_to_virtual_cap_pools!(school)
+        school_device_allocation.reload
+      end
+
+      # Precondition
+      context 'precondition' do
+        let(:initial_value) { 1 }
+        let(:updated_value) { 2 }
+
+        it 'school_device_allocation#is_in_virtual_cap_pool? is true' do
+          expect(school_device_allocation.is_in_virtual_cap_pool?).to be true
+        end
+      end
+
+      context 'when increasing the allocation' do
+        let(:initial_value) { 1 }
+        let(:updated_value) { 2 }
+
+        it 'is valid' do
+          expect(form).to be_valid
+        end
+      end
+
+      context 'when decreasing the allocation' do
+        let(:initial_value) { 2 }
+        let(:updated_value) { 1 }
+
+        it 'has errors' do
+          expect(form).to be_invalid
+          expect(form.errors[:allocation]).to be_present
+          expect(form.errors[:allocation].first).to include('Decreasing an allocation for a school in a virtual cap pool is currently not possible')
+        end
+      end
+    end
+
+    context 'compared with raw_devices_ordered' do
+      let(:school_device_allocation) { create(:school_device_allocation, allocation: 3, devices_ordered: 2) }
+
+      context 'when allocation is above raw_devices_ordered' do
+        subject(:form) { described_class.new(allocation: 4, school_allocation: school_device_allocation) }
+
+        it 'is valid' do
+          expect(form).to be_valid
+        end
+      end
+
+      context 'when allocation is below raw_devices_ordered' do
+        subject(:form) { described_class.new(allocation: 1, school_allocation: school_device_allocation) }
+
+        it 'has errors' do
+          expect(form).to be_invalid
+          expect(form.errors[:allocation]).to be_present
+          expect(form.errors[:allocation].first).to include('Allocation cannot be less than the number they have already ordered')
+        end
+      end
+    end
+  end
+end
