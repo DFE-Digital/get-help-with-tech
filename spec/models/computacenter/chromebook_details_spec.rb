d5e185a2b815dc48adfdd757c9c84ef7826fd623
Kenneth Lee <kenfodder@gmail.com>
2021-05-25 16:26:38 +0100

    List yes and i_dont_know schools in the chromebooks.csv

--- spec/models/computacenter/chromebook_details_spec.rb
+++ spec/models/computacenter/chromebook_details_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe Computacenter::ChromebookDetails do
   subject(:service) { described_class }
 
   describe '.to_csv' do
-    let(:chromebook_details) { create_list(:preorder_information, 5, :needs_chromebooks) }
+    let!(:chromebook_details) do
+      create_list(:preorder_information, 5, :needs_chromebooks)
+        .concat(create_list(:preorder_information, 2, :dont_know_they_need_chromebooks))
+    end
+
+    before { create_list(:preorder_information, 3, :does_not_need_chromebooks) }
 
     it 'has correct headers set' do
       rows = CSV.parse(service.to_csv)
@@ -12,17 +17,24 @@ RSpec.describe Computacenter::ChromebookDetails do
       expect(rows[0]).to eql(Computacenter::ChromebookDetails::HEADERS)
     end
 
-    it 'includes all the chromebook details' do
+    it 'has correct number of rows' do
+      rows = CSV.parse(service.to_csv)
+
+      expect(rows.size).to be(8)
+    end
+
+    it 'includes all the chromebook details for will_need_chromebooks yes and i_dont_know only' do
       details = details_to_array
       rows = CSV.parse(service.to_csv)
-      (1..5).each do |n|
+
+      (1..7).each do |n|
         expect(rows[n]).to eql(details[n - 1])
       end
     end
 
     def details_to_array
       details = []
-      chromebook_details.sort_by(&:id).each do |cb|
+      chromebook_details.sort_by(&:updated_at).each do |cb|
         details << [
           cb.school.responsible_body.computacenter_identifier,
           cb.school.responsible_body.name,
