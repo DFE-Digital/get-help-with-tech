90cbebc5503974fabaf714d0499fd914b8640a08
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-05-27 17:38:36 +0100

    Fix problem with both school and RB users seeing Huawei password

--- app/controllers/responsible_body/internet/huawei_router_passwords_controller.rb
+++
@@ -1,5 +0,0 @@
-class ResponsibleBody::Internet::HuaweiRouterPasswordsController < ResponsibleBody::BaseController
-  def new
-    @password = Settings.huawei.devices.password
-  end
-end
--- app/controllers/school/internet/huawei_router_passwords_controller.rb
+++
@@ -1,5 +0,0 @@
-class School::Internet::HuaweiRouterPasswordsController < School::BaseController
-  def new
-    @password = Settings.huawei.devices.password
-  end
-end
