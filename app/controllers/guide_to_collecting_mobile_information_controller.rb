2ef2d0ab7b2551fb6a690341de0cda7341150067
Jits <dev@jits.co.uk>
2021-06-28 16:25:18 +0100

    [2151] Update guidance content: /internet-access – part 2
    <https://trello.com/c/Foplspxv/2151-update-guidance-content-internet-access>
    
    Follow up after feedback on: <https://github.com/DFE-Digital/get-help-with-tech/pull/1818>

--- app/controllers/guide_to_collecting_mobile_information_controller.rb
+++ app/controllers/guide_to_collecting_mobile_information_controller.rb
@@ -2,8 +2,6 @@ class GuideToCollectingMobileInformationController < ApplicationController
   include Rails.application.routes.url_helpers
   before_action :set_guide_navigation
 
-  def asking_for_account_holder; end
-
   def privacy; end
 
 private
@@ -13,32 +11,14 @@ private
     current_index = @contents.find_index { |item| item[:path] == request.path }
 
     @contents[current_index][:active] = true
-    @next_page = contents[current_index + 1] || false
-    @prev_page = current_index.positive? ? contents[current_index - 1] : false
     @title = contents[current_index][:text]
   end
 
   def contents
     [
       {
-        text: t('guide_to_collecting_mobile_information.overview'),
-        path: guide_to_collecting_mobile_information_path,
-      },
-      {
-        text: t('guide_to_collecting_mobile_information.asking_for_network'),
-        path: guide_to_collecting_mobile_information_asking_for_network_path,
-      },
-      {
-        text: t('guide_to_collecting_mobile_information.telling_about_offer'),
-        path: guide_to_collecting_mobile_information_telling_about_offer_path,
-      },
-      {
         text: t('guide_to_collecting_mobile_information.privacy'),
         path: guide_to_collecting_mobile_information_privacy_path,
-      },
-      {
-        text: t('guide_to_collecting_mobile_information.asking_for_account_holder'),
-        path: guide_to_collecting_mobile_information_asking_for_account_holder_path,
       },
     ]
   end
