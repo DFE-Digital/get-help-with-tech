c64b1890e8859ada3d978e7e8978954e0190ba43
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-06-29 14:13:32 +0100

    Rename component to better reflect its new behaviour

--- app/components/display_devices_ordered_component.rb
+++ app/components/display_devices_ordered_component.rb
@@ -1,13 +1,13 @@
-class DisplayAllocationsComponent < ViewComponent::Base
+class DisplayDevicesOrderedComponent < ViewComponent::Base
   attr_reader :school
 
   def initialize(school:)
     @school = school
   end
 
-  def allocations
-    std_devices_count = school.std_device_allocation&.raw_allocation.to_i
-    coms_devices_count = school.coms_device_allocation&.raw_allocation.to_i
+  def devices_ordered
+    std_devices_count = school.std_device_allocation&.raw_devices_ordered || 0
+    coms_devices_count = school.coms_device_allocation&.raw_devices_ordered || 0
     [
       build_text(std_devices_count, 'device'),
       build_text(coms_devices_count, 'router'),
