10c7afc558c8179bc7b7e0e6ded30056ba0e80ed
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-08-05 11:34:42 +0100

    Add class to encrypt/decrypt strings (for database fields)

---
+++ lib/encryption_service.rb
@@ -0,0 +1,35 @@
+class EncryptionService
+  # for our enviornments, generate one-off environment variables:
+  #
+  # * ENCRYPTION_SERVICE_KEY
+  # * ENCRYPTION_SERVICE_SALT
+  #
+  # with `SecureRandom.hex(12)` or suchlike
+
+  def initialize
+    @key = ActiveSupport::KeyGenerator.new(
+      ENV.fetch('GHWT__DATABASE_FIELD_ENCRYPTION__KEY'),
+    ).generate_key(
+      ENV.fetch('GHWT__DATABASE_FIELD_ENCRYPTION__SALT'),
+      ActiveSupport::MessageEncryptor.key_len,
+    )
+
+    freeze
+  end
+
+  delegate :encrypt_and_sign, :decrypt_and_verify, to: :encryptor
+
+  def self.encrypt(plaintext)
+    new.encrypt_and_sign(plaintext)
+  end
+
+  def self.decrypt(ciphertext)
+    new.decrypt_and_verify(ciphertext)
+  end
+
+private
+
+  def encryptor
+    ActiveSupport::MessageEncryptor.new(@key)
+  end
+end
