e3d0ba711a3ab2bf1241b37250b00de3d9a0e4e9
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-08-12 12:13:18 +0100

    Add job for importing CC assets

---
+++ spec/jobs/computacenter_historical_asset_import_job_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe ComputacenterHistoricalAssetImportJob, type: :job do
+  let(:sys_id_1) { '62c7' }
+  let(:asset_tag_1) { '30000000' }
+  let(:serial_number_1) { '1FFFFFF' }
+  let(:model_name_1) { 'Acme Laptop 1000' }
+  let(:department_name_1) { 'X ACADEMY TRUST' }
+  let(:department_id_1) { 't9000000' }
+  let(:sold_to_1) { '81000000' }
+  let(:location_name_1) { 'X Academy' }
+  let(:location_id_1) { 'L1' }
+  let(:ship_to_1) { '81000000' }
+  let(:bios_password_1) { 'secretbiospassword1' }
+  let(:admin_password_1) { 'secretadminpassword1' }
+  let(:hardware_hash_1) { 'secrethardwarehash1' }
+  let(:sys_created_on_1) { '2020-11-23 14:03:04' }
+
+  let(:sys_id_2) { '7bd4' }
+  let(:asset_tag_2) { '30000001' }
+  let(:serial_number_2) { '1FFFFF1' }
+  let(:model_name_2) { 'Acme Laptop 2000' }
+  let(:department_name_2) { 'Y ACADEMY TRUST' }
+  let(:department_id_2) { 't9000001' }
+  let(:sold_to_2) { '81000001' }
+  let(:location_name_2) { 'Y Academy' }
+  let(:location_id_2) { 'L2' }
+  let(:ship_to_2) { '81000001' }
+  let(:bios_password_2) { 'secretbiospassword2' }
+  let(:admin_password_2) { 'secretadminpassword2' }
+  let(:hardware_hash_2) { 'secrethardwarehash2' }
+  let(:sys_created_on_2) { '2020-12-24 18:03:04' }
+
+  let(:header_row) { ['sys_id', 'asset_tag', 'serial_number', 'model.display_name', 'department.name', 'department.id', 'department.u_sold_to_id', 'location.name', 'location.name.u_location_id', 'location.u_cc_ship_to_account', 'u_bios_password', 'u_admin_password', 'u_hardware_hash', 'sys_created_on'] }
+  let(:row_1) { [sys_id_1, asset_tag_1, serial_number_1, model_name_1, department_name_1, department_id_1, sold_to_1, location_name_1, location_id_1, ship_to_1, bios_password_1, admin_password_1, hardware_hash_1, sys_created_on_1] }
+  let(:row_2) { [sys_id_2, asset_tag_2, serial_number_2, model_name_2, department_name_2, department_id_2, sold_to_2, location_name_2, location_id_2, ship_to_2, bios_password_2, admin_password_2, hardware_hash_2, sys_created_on_2] }
+
+  let(:asset_csv_file_path) { 'assets.csv' }
+
+  describe '#perform' do
+    describe 'read file' do
+      before do
+        allow(File).to receive(:open)
+        described_class.perform_now(asset_csv_file_path)
+      end
+
+      specify { expect(File).to have_received(:open).with(asset_csv_file_path, 'r') }
+    end
+  end
+
+  describe '#perform_on_file' do
+    let(:test_io) { StringIO.new }
+    let(:job) { described_class.new }
+
+    before do
+      # behaves as CSV does when CSV options say file contains a header row
+      allow(CSV).to receive(:foreach).and_yield(row_1).and_yield(row_2)
+      allow(job).to receive(:unix_word_count_output).and_return("      3 #{asset_csv_file_path}\n")
+    end
+
+    describe 'record creation' do
+      it 'creates two records' do
+        expect { job.perform_on_file(test_io) }.to change { Asset.count }.from(0).to(2)
+        expect(Asset.first).to have_attributes(tag: asset_tag_1, serial_number: serial_number_1, model: model_name_1, department: department_name_1, department_id: department_id_1, department_sold_to_id: sold_to_1, location: location_name_1, location_id: location_id_1, location_cc_ship_to_account: ship_to_1, bios_password: bios_password_1, admin_password: admin_password_1, hardware_hash: hardware_hash_1, sys_created_at: Time.zone.parse(sys_created_on_1))
+        expect(Asset.second).to have_attributes(tag: asset_tag_2, serial_number: serial_number_2, model: model_name_2, department: department_name_2, department_id: department_id_2, department_sold_to_id: sold_to_2, location: location_name_2, location_id: location_id_2, location_cc_ship_to_account: ship_to_2, bios_password: bios_password_2, admin_password: admin_password_2, hardware_hash: hardware_hash_2, sys_created_at: Time.zone.parse(sys_created_on_2))
+      end
+    end
+
+    describe 'logging' do
+      let(:filename_regex_suitable_for_string_io_class) { /\.*/ }
+
+      before do
+        allow(Rails.logger).to receive(:info)
+        job.perform_on_file(test_io)
+      end
+
+      it 'logs' do
+        expect(Rails.logger).to have_received(:info).with(/Started ComputacenterHistoricalAssetImportJob for #{filename_regex_suitable_for_string_io_class.source} with 2 asset\(s\)/).ordered
+        expect(Rails.logger).to have_received(:info).with(/Finished ComputacenterHistoricalAssetImportJob for #{filename_regex_suitable_for_string_io_class.source} with 2 asset\(s\) imported/).ordered
+        expect(Rails.logger).to have_received(:info).with('There are now 2 total asset(s) in the database').ordered
+      end
+    end
+  end
+end
