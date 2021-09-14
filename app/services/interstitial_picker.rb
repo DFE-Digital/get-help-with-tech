63c710b34b6d219708e0671e92834e2ef5d6d5ff
Kenneth Lee <kenfodder@gmail.com>
2021-09-14 12:21:50 +0100

    Revert "Change content"
    This reverts commit 8ff61e442055cbee1b87f7b991131b56fd0ddc87.

--- app/services/interstitial_picker.rb
+++ app/services/interstitial_picker.rb
@@ -6,20 +6,51 @@ class InterstitialPicker
   end
 
   def call
-    @call ||= if user.is_support? || user.is_mno_user?
+    @call ||= if rb_user_with_la_funded_place_with_available_devices? || user_with_multiple_la_funded_places_with_available_devices?
+                OpenStruct.new(
+                  title: 'Laptops and internet access are available for more children and young people',
+                  partial: 'interstitials/la_funded_user',
+                )
+              elsif iss_provision_user_with_available_devices?
+                OpenStruct.new(
+                  title: 'Get laptops and internet access',
+                  partial: 'interstitials/iss_provision_user',
+                )
+              elsif scl_provision_user_with_available_devices?
+                OpenStruct.new(
+                  title: 'Get laptops and internet access',
+                  partial: 'interstitials/scl_provision_user',
+                )
+              elsif user.is_school_user?
                 OpenStruct.new(
                   title: title_for_default,
-                  partial: 'interstitials/default',
+                  partial: 'interstitials/school_user',
                 )
               else
                 OpenStruct.new(
                   title: title_for_default,
-                  partial: 'interstitials/school_user',
+                  partial: 'interstitials/default',
                 )
               end
   end
 
 private
+
+  def rb_user_with_la_funded_place_with_available_devices?
+    user.la_funded_user? && user.is_responsible_body_user? && user.responsible_body.schools.la_funded_provision.any? { |school| school.device_allocations&.can_order_std_devices_now.present? }
+  end
+
+  def user_with_multiple_la_funded_places_with_available_devices?
+    user.schools.la_funded_provision.count > 1 && user.schools.la_funded_provision.any? { |school| school.device_allocations&.can_order_std_devices_now.present? }
+  end
+
+  def iss_provision_user_with_available_devices?
+    user.iss_provision_user? && user.schools.iss_provision.first&.device_allocations&.can_order_std_devices_now.present?
+  end
+
+  def scl_provision_user_with_available_devices?
+    user.scl_provision_user? && user.schools.scl_provision.first&.device_allocations&.can_order_std_devices_now.present?
+  end
 
   def title_for_default
     i18n_key = user.is_school_user? || (user.responsible_body_user? && !user.single_school_user?) ? :related_organisation : :standard
