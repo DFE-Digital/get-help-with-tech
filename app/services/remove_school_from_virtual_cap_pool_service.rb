bdc08a93ef209e53ed6da03871cc9473a522614a
Lorenzo Tello <ltello8a@gmail.com>
2021-09-28 15:10:26 +0200

    2340: remove comments

--- app/services/remove_school_from_virtual_cap_pool_service.rb
+++ app/services/remove_school_from_virtual_cap_pool_service.rb
@@ -17,9 +17,7 @@ class RemoveSchoolFromVirtualCapPoolService
 private
 
   def remove_device_allocation_from_pool!(allocation_id)
-    byebug
     SchoolVirtualCap.find_by(school_device_allocation_id: allocation_id)&.destroy!
-    # &.recalculate_caps!
   end
 
   def remove_devices_from_pools!
@@ -37,9 +35,7 @@ private
 
   def remove_school!
     school.transaction do
-      byebug
       remove_devices_from_pools!
-      # rb.calculate_virtual_caps!
       school.reload.refresh_device_ordering_status!
       true
     end
