a8c930e675e0b059f3a551f4db2f855552b3b865
Lorenzo Tello <ltello8a@gmail.com>
2021-10-05 15:53:30 +0200

    2222: add missing tests for device count

--- app/concerns/device_count.rb
+++ app/concerns/device_count.rb
@@ -10,13 +10,7 @@ module DeviceCount
       [0, devices_available_to_order_or_over_ordered].max
     end
 
-    def devices_over_ordered?
-      devices_available_to_order_or_over_ordered.negative?
-    end
-
-    def devices_over_ordered
-      [0, devices_available_to_order_or_over_ordered].min
-    end
+    private
 
     def devices_available_to_order_or_over_ordered
       cap.to_i - devices_ordered.to_i
--- spec/models/school_device_allocation_spec.rb
+++ spec/models/school_device_allocation_spec.rb
@@ -54,11 +54,49 @@ RSpec.describe SchoolDeviceAllocation, type: :model do
   end
 
   describe '#devices_available_to_order' do
-    subject(:allocation) { build_stubbed(:school_device_allocation, cap: 1, devices_ordered: 2) }
+    subject(:allocation) { build_stubbed(:school_device_allocation, cap: cap, devices_ordered: 2) }
 
-    context 'when negative' do
+    context 'when more devices ordered than the assigned cap' do
+      let(:cap) { 1 }
+
       it 'returns zero' do
         expect(allocation.devices_available_to_order).to be_zero
+      end
+    end
+
+    context 'when no more devices ordered than the assigned cap' do
+      let(:cap) { 3 }
+
+      it 'returns the difference' do
+        expect(allocation.devices_available_to_order).to eq(1)
+      end
+    end
+  end
+
+  describe '#devices_available_to_order?' do
+    subject(:allocation) { build_stubbed(:school_device_allocation, cap: cap, allocation: cap, devices_ordered: 1) }
+
+    context 'when used full allocation' do
+      let(:cap) { 1 }
+
+      it 'returns false' do
+        expect(allocation.devices_available_to_order?).to be false
+      end
+    end
+
+    context 'when used over allocation' do
+      let(:cap) { 0 }
+
+      it 'returns false' do
+        expect(allocation.devices_available_to_order?).to be false
+      end
+    end
+
+    context 'when partially used allocation' do
+      let(:cap) { 2 }
+
+      it 'returns true' do
+        expect(allocation.devices_available_to_order?).to be true
       end
     end
   end
