d5cd9b35ef0dac747b67561a38d3e4e57d80099e
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-10-14 11:54:48 +0100

    Content changes for "Order devices"

--- spec/features/responsible_body/ordering_devices_in_pool_spec.rb
+++ spec/features/responsible_body/ordering_devices_in_pool_spec.rb
@@ -121,7 +121,7 @@ RSpec.feature 'Ordering devices within a virtual pool' do
   end
 
   def then_i_see_the_get_laptops_and_tablets_page
-    expect(page).to have_css('h1', text: 'Get devices')
+    expect(page).to have_css('h1', text: 'Order devices')
     expect(page).to have_link('Your schools')
     expect(page).to have_link('Order devices')
   end
