16acfe51f59af03d9a6eb4c2d83a329848ff94d1
Nigel Lowry <nigel.lowry@digital.education.gov.uk>
2021-10-19 09:10:06 +0100

    Fix Pagy upgrade

--- config/initializers/pagination.rb
+++ config/initializers/pagination.rb
@@ -2,5 +2,5 @@ require 'pagy'
 require 'pagy/extras/items'
 require 'pagy/extras/i18n'
 
-Pagy::VARS[:items] = 100
-Pagy::VARS[:max_items] = 9999
+Pagy::DEFAULT[:items] = 100
+Pagy::DEFAULT[:max_items] = 9999
