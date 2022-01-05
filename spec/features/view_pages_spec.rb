dcb46fea9273c4869b547e1734584393850659fc
Kenneth Lee <kenfodder@gmail.com>
2022-01-05 13:31:23 +0000

    Reinstate view pages spec and clean false routes

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature 'View pages', type: :feature, skip: 'Disabled for 30 Jun 2021 service closure' do
+RSpec.feature 'View pages', type: :feature do
   scenario 'Root URL should be the guidance page' do
     visit '/'
-    expect(page).to have_selector 'h1', text: I18n.t('service_name')
-  end
-
-  scenario 'Navigate to guidance page' do
-    visit '/pages/guidance'
-
-    expect(page).to have_http_status(:ok)
     expect(page).to have_selector 'h1', text: I18n.t('service_name')
   end
 
