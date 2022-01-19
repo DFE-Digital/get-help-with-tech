ce580a038e0f7ec6cfa7eeddfe69d3f1e8b0d8d8
Lorenzo Tello <ltello8a@gmail.com>
2022-01-19 10:51:25 +0100

    2674: little refactor

--- app/controllers/support/allocation_batch_jobs_controller.rb
+++ app/controllers/support/allocation_batch_jobs_controller.rb
@@ -1,17 +1,20 @@
 class Support::AllocationBatchJobsController < Support::BaseController
   before_action { authorize AllocationBatchJob }
 
+  attr_reader :batch_id
+
   def index
     @batches = AllocationBatchJob.select('batch_id, max(created_at) as created_at').group(:batch_id).order('max(created_at) desc')
   end
 
   def show
-    jobs = AllocationBatchJob.where(batch_id: params[:id])
-    @pagination, @jobs = pagy(jobs.select('ABS(allocation_delta::INTEGER - applied_allocation_delta::INTEGER) delta_mismatch, *')
+    @batch_id = params[:id]
+    query = AllocationBatchJob.where(batch_id: batch_id)
+    @pagination, @jobs = pagy(query.select('ABS(allocation_delta::INTEGER - applied_allocation_delta::INTEGER) delta_mismatch, *')
                                   .order('delta_mismatch desc, urn, ukprn'))
-    @aggregate_allocation_change = jobs.sum(:applied_allocation_delta)
-    @number_of_processed_jobs = jobs.where(processed: true).size
-    @total_number_of_jobs = jobs.size
+    @aggregate_allocation_change = query.sum(:applied_allocation_delta)
+    @number_of_processed_jobs = query.where(processed: true).size
+    @total_number_of_jobs = query.size
   end
 
   def send_notifications
