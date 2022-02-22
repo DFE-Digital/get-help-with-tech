c153476e4e3890f9c66ad20f850aa137ff0fdafd
Lorenzo Tello <ltello8a@gmail.com>
2022-02-22 09:12:22 +0100

    2726: make migrations reversible

--- db/migrate/20200701140641_disallow_null_recipient_status.rb
+++ db/migrate/20200701140641_disallow_null_recipient_status.rb
@@ -1,6 +1,10 @@
 class DisallowNullRecipientStatus < ActiveRecord::Migration[6.0]
-  def change
+  def up
     Recipient.where(status: nil).update_all(status: Recipient.statuses[:requested])
     change_column :recipients, :status, :string, null: false
+  end
+
+  def down
+    change_column :recipients, :status, :string, null: true
   end
 end
