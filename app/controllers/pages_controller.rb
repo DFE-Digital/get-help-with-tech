8fb5dab16d37d3153b3da2ce2edbd86e8864010c
Lorenzo Tello <ltello8a@gmail.com>
2022-02-25 17:09:12 +0100

    2768: set managing 4g wireless routers as a main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,13 +13,15 @@ class PagesController < ApplicationController
 
   def dfe_windows_privacy_notice; end
 
-  def general_privacy_notice; end
+  def finding_out_about_internet_access_needs; end
 
-  def request_a_change; end
+  def general_privacy_notice; end
 
   def internet_access
     redirect_to root_path
   end
 
-  def finding_out_about_internet_access_needs; end
+  def managing_your_4g_wireless_routers; end
+
+  def request_a_change; end
 end
