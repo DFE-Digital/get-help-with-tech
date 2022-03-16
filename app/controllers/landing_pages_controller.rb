bbc6a886ba0eb4da69fe5411745a5b9cff11a771
Kenneth Lee <kenfodder@gmail.com>
2022-03-16 17:15:32 +0000

    Remove digital platforms page

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,8 +1,3 @@
 class LandingPagesController < ApplicationController
   layout 'page_with_toc'
-
-  def digital_platforms
-    @title = I18n.t!('landing_pages.digital_platforms.title')
-    render
-  end
 end
