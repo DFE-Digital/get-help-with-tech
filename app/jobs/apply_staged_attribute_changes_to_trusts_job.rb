e7b0aa6fb9061e0a39afd11ef7a9c3ad78b77a32
Lorenzo Tello <ltello8a@gmail.com>
2022-05-06 13:24:47 +0200

    2929: pausing ApplyStagedAttributeChangesToTrustsJob

--- app/jobs/apply_staged_attribute_changes_to_trusts_job.rb
+++ app/jobs/apply_staged_attribute_changes_to_trusts_job.rb
@@ -1,5 +1,11 @@
 class ApplyStagedAttributeChangesToTrustsJob < ApplicationJob
   def perform
-    TrustUpdateService.new.update_trusts
+    TrustUpdateService.new.update_trusts unless paused?
+  end
+
+private
+
+  def paused?
+    FeatureFlag.active?(:gias_data_stage_pause)
   end
 end
