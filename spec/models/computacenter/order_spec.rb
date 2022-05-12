9a0d86f16f3983609c68a20538386970d142e061
Mark Allen <markhallen@gmail.com>
2022-05-12 16:52:16 +0100

    Fix order spec

--- spec/models/computacenter/order_spec.rb
+++ spec/models/computacenter/order_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Computacenter::Order, type: :model do
-  it 'returns an Order' do
-    let(:order) { build_stubbed(:computacenter_order) }
+  let(:order) { build_stubbed(:computacenter_order) }
 
+  it 'returns an Order' do
     expect(order).to be_a(described_class)
   end
 
