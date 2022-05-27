525c7b47b866372b42a9da45691c9b7eb9064bf4
Lorenzo Tello <ltello8a@gmail.com>
2022-05-27 14:29:47 +0200

    update a few radio buttons on prism pages

--- spec/page_objects/support_ticket/support_needs_page.rb
+++ spec/page_objects/support_ticket/support_needs_page.rb
@@ -4,13 +4,13 @@ module PageObjects
       set_url '/get-support/support-needs'
 
       element :heading, '.govuk-fieldset__legend--xl'
-      element :laptops_checkbox_option, '#support-ticket-support-needs-form-support-topics-laptops-and-tablets-field'
+      element :laptops_checkbox_option, '#new_support_ticket_support_needs_form #laptops_and_tablets_option'
       # element :laptops_suggestions, '#support-ticket-support-needs-form-support-topics-laptops-and-tablets-conditional'
-      element :routers_checkbox_option, '#support-ticket-support-needs-form-support-topics-4g-wireless-routers-and-internet-access-field'
+      element :routers_checkbox_option, '#new_support_ticket_support_needs_form #4g_wireless_routers_and_internet_access_option'
       # element :routers_suggestions, '#support-ticket-support-needs-form-support-topics-4g-wireless-routers-and-internet-access-conditional'
-      element :platforms_checkbox_option, '#support-ticket-support-needs-form-support-topics-digital-education-platforms-field'
-      element :training_and_support_checkbox_option, '#support-ticket-support-needs-form-support-topics-technology-training-and-support-field'
-      element :something_else_checkbox_option, '#support-ticket-support-needs-form-support-topics-something-else-field'
+      element :platforms_checkbox_option, '#new_support_ticket_support_needs_form #digital_education_platforms_option'
+      element :training_and_support_checkbox_option, '#new_support_ticket_support_needs_form #technology_training_and_support_option'
+      element :something_else_checkbox_option, '#new_support_ticket_support_needs_form #something_else_option'
       element :continue_button, :button, text: 'Continue'
 
       def select_anything_and_continue
